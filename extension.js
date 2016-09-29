267e72c225abc70c3f0ada23ae261e6a9b79b5cc
Logan Hampton <losesmc@gmail.com>
2016-09-29 12:04:21 -0700

    Implemented running scripts from VS Code
    Right now it assumes that you have AutoIt installed in the standard directory on a 64-bit system.

---
+++ extension.js
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+var vscode = require('vscode');
+var launch = require('child_process').execFile;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+
+    var aiPath = "C:\\Program Files (x86)\\AutoIt3\\AutoIt3.exe"
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('AutoIt is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World!');
+    });
+
+    var runScript = vscode.commands.registerCommand('extension.runScript', function () {
+        
+        //Get the current file name
+        var thisFile = vscode.window.activeTextEditor.document.fileName;
+        //Launch the AutoIt executable with the script's path
+        launch(aiPath, [thisFile], (err, stdout, stderr) => {
+            if (err) {
+                vscode.window.showErrorMessage(err);
+                return;
+            }
+        });
+    });
+
+
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(runScript);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
