358400542ed7f2f3a270f224cb305a4b8cfa792e
Logan Hampton <losesmc@gmail.com>
2017-01-10 00:58:46 -0700

    Move commands to separate module

---
+++ out/src/ai_commands.js
@@ -0,0 +1,97 @@
+var { window, Position } = require('vscode');
+var launch = require('child_process').execFile;
+
+// Executable paths
+const aiPath = "C:\\Program Files (x86)\\AutoIt3\\AutoIt3.exe";
+const wrapperPath = "C:\\Program Files (x86)\\AutoIt3\\SciTE\\AutoIt3Wrapper\\AutoIt3Wrapper.au3";
+var helpPath = "C:\\Program Files (x86)\\AutoIt3\\AutoIt3Help.exe";
+
+module.exports = {
+    
+    runScript: () => {
+        var thisDoc = window.activeTextEditor.document; // Get the object of the text editor
+        var thisFile = thisDoc.fileName; // Get the current file name
+        
+        // Save the file
+        thisDoc.save();
+
+        window.setStatusBarMessage("Running the script...", 1500);
+        // Launch the AutoIt executable with the script's path
+        launch(aiPath, [thisFile, '/run', '/prod', '/ErrorStdOut', '/in'], (err, stdout, stderr) => {
+            if (err) {
+                window.showErrorMessage(err);
+                console.log(stderr);
+                return;
+            }
+            
+            console.log(stdout);
+        });
+    },
+
+    launchHelp: () => {
+        var editor = window.activeTextEditor;
+
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        // Get the selected text and launch it
+        var selection = editor.selection;
+        var query = editor.document.getText(selection).trim();
+
+        window.setStatusBarMessage(`Searching documentation for ${query}`, 1500);
+
+        launch(helpPath, [query]);
+    },
+
+    launchInfo: () => {
+        launch('C:\\Program Files (x86)\\AutoIt3\\Au3Info.exe');   
+    },
+
+    debugMsgBox: () => {
+        var editor = window.activeTextEditor;
+
+        if (!editor) {
+            return; // No open editor
+        }
+
+        var selection = editor.selection;
+        var varToDebug = editor.document.getText(selection).trim();
+
+        // Make sure that a variable or macro is selected
+        if (varToDebug.charAt(0) === '$' || varToDebug.charAt(0) === '@') {
+            // Get the value of the next line after selected variable for the new MsgBox
+            var nextLine = editor.selection.active.line + 1;
+            var newPosition = new Position(nextLine, 0);
+
+            var debugCode = "MsgBox(262144, 'Debug line ~' & @ScriptLineNumber - 1, 'Selection:' & @CRLF & '";
+            debugCode +=  varToDebug + "' & @CRLF & @CRLF & 'Return:' & @CRLF & " + varToDebug;
+            debugCode += ") ;### Debug MSGBOX\n";
+
+            // Insert the code for the MsgBox into the script
+            editor.edit(edit => {
+                edit.insert(newPosition, debugCode);
+            });
+        } else {
+            window.showErrorMessage("Select variable or macro to generate a Debug MsgBox");
+            return;
+        }
+    },
+
+    compileScript: () => {
+        // Save the file
+        window.activeTextEditor.document.save();
+        // Get the current file name
+        var thisFile = window.activeTextEditor.document.fileName;
+
+        // Launch the AutoIt Wrapper executable with the script's path
+        launch(aiPath, [wrapperPath, '/prod', '/in', thisFile], (err, stdout, stderr) => {
+            if (err) {
+                window.showErrorMessage(stderr);
+                console.log(stderr);
+                return;
+            }
+            console.log(stdout);
+        })
+    }
+};
--- out/src/extension.js
+++ out/src/extension.js
@@ -1,105 +1,49 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
-var launch = require('child_process').execFile;
+var { languages, Hover, commands } = require('vscode');
+var ai_commands  = require("./ai_commands");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
+function activate(ctx) {
 
-    var aiPath = "C:\\Program Files (x86)\\AutoIt3\\AutoIt3.exe";
-    var wrapperPath = "C:\\Program Files (x86)\\AutoIt3\\SciTE\\AutoIt3Wrapper\\AutoIt3Wrapper.au3";
-    var helpPath = "C:\\Program Files (x86)\\AutoIt3\\AutoIt3Help.exe";
+    ctx.subscriptions.push(languages.registerHoverProvider({ language: 'autoit', scheme: 'file' }, {
+        provideHover(document, position, token) {
+            let wordRange = document.getWordRangeAtPosition(position);
+	        let lineText = document.lineAt(position.line).text;
+            let word = wordRange ? document.getText(wordRange) : '';
+            let hover;
+
+            switch(word) {
+                case 'ClipGet':
+                    hover = "(function) ClipGet() - Retrieves text from the clipboard."        
+                    break;
+                default:
+                    
+                    break;
+            }
+            return new Hover(hover);
+        }
+    }));
+    
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('AutoIt is now active!');
 
-    var runScript = vscode.commands.registerCommand('extension.runScript', function () {
-        
-        //Save the file
-        vscode.window.activeTextEditor.document.save();
-        //Get the current file name
-        var thisFile = vscode.window.activeTextEditor.document.fileName;
-        //Launch the AutoIt executable with the script's path
-        launch(aiPath, [thisFile], (err, stdout, stderr) => {
-            if (err) {
-                vscode.window.showErrorMessage(err);
-                return;
-            }
-        });
-    });
-
-    var launchHelp = vscode.commands.registerCommand('extension.launchHelp', function () {
-        
-        var editor = vscode.window.activeTextEditor;
-
-        if (!editor) {
-            return; // No open text editor
-        }
-
-        //Get selected text and launch it
-        var selection = editor.selection;
-        var query = editor.document.getText(selection);
-
-        vscode.window.setStatusBarMessage("Searching documentation for " 
-            + query, 1500);
-
-        launch(helpPath, [query]);
-    });
-
-    var launchInfo = vscode.commands.registerCommand('extension.launchInfo', function () {
-
-        launch('C:\\Program Files (x86)\\AutoIt3\\Au3Info.exe')
-    });
-
-    //Insert a Debug MsgBox
-    var debugMsgBox = vscode.commands.registerCommand('extension.debugMsgBox', function () {
-        var editor = vscode.window.activeTextEditor;
-
-        if  (!editor) {
-            return; // No open text editor
-        }
-
-        var selection = editor.selection;
-        var varToDebug = editor.document.getText(selection).trim();
-        
-        //Make sure that a variable or macro is selected
-        if (varToDebug.charAt(0) === '$' || varToDebug.charAt(0) === '@') {
-            // Get the value of the next line after selected variable for the new MsgBox
-            var nextLine = editor.selection.active.line + 1;
-            var newPosition = new vscode.Position(nextLine, 0);
-            
-            var debugCode = "MsgBox(262144, 'Debug line ~' & @ScriptLineNumber - 1, 'Selection:' & @CRLF & '";
-            debugCode +=  varToDebug + "' & @CRLF & @CRLF & 'Return:' & @CRLF & " + varToDebug
-            debugCode += ") ;### Debug MSGBOX\n";
-
-            //Insert the code for the MsgBox into the script
-            editor.edit(edit => {
-                edit.insert(newPosition, debugCode)
-            });
-        } else {
-            vscode.window.showErrorMessage("Select a variable or macro to generate a Debug MessageBox");
-            return;
-        }
-    });
-
-    //Compile scripts
-    var compile = vscode.commands.registerCommand('extension.compile', function () {
-        //Save the file
-        vscode.window.activeTextEditor.document.save();
-        //Get the current file name
-        var thisFile = vscode.window.activeTextEditor.document.fileName;
-        
-        //Launch the AutoIt Wrapper executable with the script's path
-        launch(aiPath, [wrapperPath, '/prod', '/in', thisFile], (err, stdout, stderr) => {
-            if (err) {
-                vscode.window.showErrorMessage(err);
-                return;
-            }
-        });
-    });
-
-    context.subscriptions.push(runScript);
+    commands.registerCommand('extension.runScript', () => {
+        ai_commands.runScript();
+    }); // Run Script Command
+    commands.registerCommand('extension.launchHelp', () => {
+        ai_commands.launchHelp();
+    }); // Launch Help Command
+    commands.registerCommand('extension.launchInfo', () => {
+        ai_commands.launchInfo();
+    }); // Launch Au3Info
+    commands.registerCommand('extension.debugMsgBox', () => {
+        ai_commands.debugMsgBox();
+    }); // Insert Debug MsgBox
+    commands.registerCommand('extension.compile', () => {
+        ai_commands.compileScript();
+    }); // Compile scripts
+    
 }
 exports.activate = activate;
 
