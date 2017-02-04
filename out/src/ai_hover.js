e6c70b033e7c50a62e9f569e56c92cbff4072886
Logan Hampton <losesmc@gmail.com>
2017-02-04 02:51:37 -0700

    Rework hovers to reside in separate files
    The aim is to limit the size of the files for when/if any changes need
    to be made. The ai_hover module will now load and combine them when run.

--- out/src/ai_hover.js
+++ out/src/ai_hover.js
@@ -1,14 +1,17 @@
 'use strict'
 
 var { languages, document, Hover } = require('vscode')
-var hovers = require('./hovers.json')
+var hovers = require('./hovers/ai_functions.json')
+var macros = require('./hovers/macros.json')
+var udf_array = require('./hovers/udf_array.json')
+
+hovers = Object.assign(hovers, macros, udf_array)
 
 module.exports = languages.registerHoverProvider(
     { language: 'autoit', scheme: 'file' },
     { 
         provideHover(document, position, token) {
             let wordRange = document.getWordRangeAtPosition(position)
-            // let lineText = document.lineAt(position.line).text
             let word = wordRange ? document.getText(wordRange) : ''
             let hover
 
