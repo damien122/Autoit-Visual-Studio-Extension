7a770f18e16c7939c523ac6ee29310011ed257e7
Logan Hampton <losesmc@gmail.com>
2017-02-06 20:18:26 -0700

    Add clipboard management udf hovers

--- out/src/ai_hover.js
+++ out/src/ai_hover.js
@@ -4,8 +4,9 @@ var { languages, document, Hover } = require('vscode')
 var hovers = require('./hovers/ai_functions.json')
 var macros = require('./hovers/macros.json')
 var udf_array = require('./hovers/udf_array.json')
+var udf_clipboard = require('./hovers/udf_clipboard.json')
 
-hovers = Object.assign(hovers, macros, udf_array)
+hovers = Object.assign(hovers, macros, udf_array, udf_clipboard)
 
 module.exports = languages.registerHoverProvider(
     { language: 'autoit', scheme: 'file' },
