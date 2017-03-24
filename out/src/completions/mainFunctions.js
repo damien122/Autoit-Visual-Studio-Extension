496c36d0793c901d502afeec5937842280fc4326
Logan Hampton <losesmc@gmail.com>
2017-03-24 02:05:51 -0700

    Further completion/snippet update

--- out/src/completions/mainFunctions.js
+++ out/src/completions/mainFunctions.js
@@ -14,6 +14,16 @@ var items = [
         insertText: new SnippetString('ACos(${expression})')
     },
     {
+        label: 'AdlibRegister',
+        documentation: 'Registers an Adlib function.',
+        insertText: new SnippetString('AdlibRegister(${"function"}, ${time})')
+    },
+    {
+        label: 'ASin',
+        documentation: 'Calculates the arcsine of a number.',
+        insertText: new SnippetString('ASin(${expression})')
+    },
+    {
         label: 'ClipGet',
         documentation: 'Path to current user\'s Roaming Application Data',
         insertText: 'ClipGet()'
@@ -23,7 +33,7 @@ var items = [
 // Add the function icon and detail to each entry
 for (var i of items) {
     i.kind = CompletionItemKind.Function
-    i.detail = 'AutoIt Function'
+    i.detail = 'Function'
 }
 
 module.exports = items
