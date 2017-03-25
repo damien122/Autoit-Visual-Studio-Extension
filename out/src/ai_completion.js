32b0af293b10d71fc192faa6e511954261ddb3ca
Logan Hampton <losesmc@gmail.com>
2017-03-24 21:18:17 -0700

    Add scan for user made functions for completion suggestions

--- out/src/ai_completion.js
+++ out/src/ai_completion.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var { languages, Completion, CompletionList, CompletionItemKind, editor } = require('vscode')
+var { languages, Completion, CompletionList, CompletionItemKind, editor, document } = require('vscode')
 var fs = require('fs')
 var completions = []
 var newComp
@@ -11,6 +11,9 @@ for (var i in files) {
     completions = completions.concat(newComp)
 }
 
+const _funcPattern = /Func\s(.+)\(/
+
+
 // var macros = require('./completions/macros')
 // var mainFunctions = require('./completions/mainFunctions')
 
@@ -18,7 +21,23 @@ module.exports = languages.registerCompletionItemProvider(
     { language: 'autoit', scheme: 'file' },
     {
         provideCompletionItems(document, position, token) {
-            
+            // Gather the functions created by the user
+            var userFunctions = []
+            var funcName = ''
+            const lineCount = Math.min(document.lineCount, 10000)
+            for (let line = 0; line < lineCount; line++) {
+                const {text}  = document.lineAt(line)
+
+                funcName = _funcPattern.exec(text)
+                if(funcName) {
+                    userFunctions.push({ label: funcName[1], kind: CompletionItemKind.Function})
+                }
+
+                if(userFunctions) {
+                    completions = completions.concat(userFunctions)
+                }
+            }
+
             // Get the current word to not look for completions 
             // when the current word starts with a '$', since
             // that blocks Intellisense for variables
