b62f2d013e445589dc4f9abed0546eb4b89f9e51
Logan Hampton <losesmc@gmail.com>
2017-04-26 21:37:33 -0700

    Add detection of variables for symbol search

--- out/src/ai_symbols.js
+++ out/src/ai_symbols.js
@@ -4,6 +4,7 @@ var { languages, SymbolInformation, SymbolKind,
     Location, Position } = require('vscode')
 
 const _funcPattern = /Func\s(.+)\(/
+const _varPattern = /(\$\w+)/
 
 module.exports = languages.registerDocumentSymbolProvider(
     { language: 'autoit', scheme: 'file' },
@@ -11,7 +12,9 @@ module.exports = languages.registerDocumentSymbolProvider(
         provideDocumentSymbols(doc, token) {
 
             let result = []
+            let found = []
             let funcName
+            let varName
 
             // Get the number of lines in the document to loop through
             const lineCount = Math.min(doc.lineCount, 10000)
@@ -19,9 +22,16 @@ module.exports = languages.registerDocumentSymbolProvider(
                 const {text}  = doc.lineAt(line)
 
                 funcName = _funcPattern.exec(text)
-                if(funcName) {
+                if(funcName && found.indexOf(funcName[1]) === -1) {
                     result.push(new SymbolInformation(funcName[1], SymbolKind.Function, 
                     '', new Location(doc.uri, new Position(line, 0))))
+                    found.push(funcName[1])
+                }
+                varName = _varPattern.exec(text)
+                if (varName && found.indexOf(varName[1]) === -1) {
+                    result.push(new SymbolInformation(varName[1], SymbolKind.Variable,
+                    '', new Location(doc.uri, new Position(line, 0))))
+                    found.push(varName[1])
                 }
             }
 
