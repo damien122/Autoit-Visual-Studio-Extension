5ee8c8182844a49e99392789fed3166c4102f247
Logan C H <losesmc@gmail.com>
2017-05-22 13:54:36 -0700

    Add line to load udf signatures

--- out/src/ai_signature.js
+++ out/src/ai_signature.js
@@ -1,43 +1,45 @@
 'use strict'
 
 var { languages, Position, SignatureHelp, SignatureInformation, ParameterInformation } = require('vscode')
+var mergeJSON = require('merge-json')
 
-var signatures = require('./signatures/functions.json')
+var mainFunctions = require('./signatures/functions.json')
+var udfs = require('./signatures/udfs.json')
 
-module.exports = languages.registerSignatureHelpProvider(
-    { language: 'autoit', scheme: 'file' },
-    {
-        provideSignatureHelp(document, position, token) {
-            // console.log("Actually getting into signature code")
-            // Find out what called for sig
-            let caller = startOfCall(document, position)
-            if (caller == null) {
-                return null
-            }
-            let callPos = previousPosition(document, caller.openParen)
-            let callWord = document.getText(document.getWordRangeAtPosition(callPos))
-            
-            //Get the called word from the json files
-            let foundSig = signatures[callWord]
-            if (foundSig == null) {
-                return null
-            }
+var signatures = mergeJSON.merge(mainFunctions, udfs)
 
-
-            // let declarationText, sig
-            let result = new SignatureHelp()
-            let si = new SignatureInformation(foundSig.label, foundSig.documentation)
-            //Enter parameter information into signature information
-            si.parameters = foundSig.params
-            
-            //Place signature information into results
-            result.signatures = [si]
-            result.activeSignature = 0
-            result.activeParameter = Math.min(caller.commas.length, si.parameters.length - 1)
-            
-            return result
+module.exports = languages.registerSignatureHelpProvider({ language: 'autoit', scheme: 'file' }, {
+    provideSignatureHelp(document, position, token) {
+        // console.log("Actually getting into signature code")
+        // Find out what called for sig
+        let caller = startOfCall(document, position)
+        if (caller == null) {
+            return null
         }
-    }, '(', ',')
+        let callPos = previousPosition(document, caller.openParen)
+        let callWord = document.getText(document.getWordRangeAtPosition(callPos))
+
+        //Get the called word from the json files
+        let foundSig = signatures[callWord]
+        if (foundSig == null) {
+            return null
+        }
+
+
+        // let declarationText, sig
+        let result = new SignatureHelp()
+        let si = new SignatureInformation(foundSig.label, foundSig.documentation)
+            //Enter parameter information into signature information
+        si.parameters = foundSig.params
+
+        //Place signature information into results
+        result.signatures = [si]
+        result.activeSignature = 0
+        result.activeParameter = Math.min(caller.commas.length, si.parameters.length - 1)
+
+        return result
+    }
+}, '(', ',')
 
 
 function startOfCall(doc, pos) {
