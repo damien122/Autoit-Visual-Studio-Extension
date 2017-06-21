2012d42928c53092028611c83bce6473fe750700
Logan Hampton <losesmc@gmail.com>
2017-06-20 23:00:36 -0700

    Enable extension to provide completions for Functions from included files

--- out/src/ai_completion.js
+++ out/src/ai_completion.js
@@ -1,9 +1,11 @@
 'use strict'
 
-var { languages, Completion, CompletionItem, CompletionItemKind, editor, document } = require('vscode')
+var { languages, Completion, CompletionItem, CompletionItemKind, editor, document, window } = require('vscode')
 var fs = require('fs')
+var path = require('path')
 var completions = []
 var newComp
+const readline = require('readline')
 
 var files = fs.readdirSync(__dirname + '/completions')
 for (var i in files) {
@@ -13,6 +15,7 @@ for (var i in files) {
 
 const _funcPattern = /Func\s+(\w*)\s*\(/g;
 const _varPattern = /\$(\w*)/g;
+const _includePattern = /#include\s"(.+)"/g
 
 module.exports = languages.registerCompletionItemProvider({ language: 'autoit', scheme: 'file' }, {
     provideCompletionItems(document, position, token) {
@@ -27,9 +30,8 @@ module.exports = languages.registerCompletionItemProvider({ language: 'autoit', 
             range = new Range(position, position);
         }
 
-        var createNewCompletionItem = function (kind, name) {
+        var createNewCompletionItem = function (kind, name, strDetail = 'Document Function') {
             var compItem = new CompletionItem(name, kind);
-            var strDetail = 'Document Function';
             if (kind == CompletionItemKind.Variable) {
                 strDetail = 'Variable';
             }
@@ -58,6 +60,46 @@ module.exports = languages.registerCompletionItemProvider({ language: 'autoit', 
             }
         }
 
+        var pattern = null
+        
+        while (pattern = _includePattern.exec(text)) {
+            var includeName = pattern[1]
+            var includeFunctions = []
+            includeFunctions = getIncludeData(includeName)
+
+            if (includeFunctions) {
+                for (var newFunc in includeFunctions) {
+                    result.push(createNewCompletionItem(CompletionItemKind.Function, includeFunctions[newFunc], includeName + ' Function'))
+                }
+            }
+        }
+
         return completions.concat(result);
     }
 }, '.', '$')
+
+function getIncludeData(fileName) {
+    // console.log(fileName)
+    const _includeFuncPattern = /^(?=\S)(?!;~\s)Func\s+(\w+)\s*\(/
+
+    var functions = []
+
+    var filePath =  path.dirname(window.activeTextEditor.document.fileName) + '\\' + fileName
+    // console.log(filePath)
+    var pattern = null
+
+    var fileData = fs.readFileSync(filePath)
+
+    var fileArray = fileData.toString().split("\n")
+
+    var funcLines = fileArray.filter((line) => {
+        pattern = _includeFuncPattern.exec(line)
+        if (pattern) {
+            functions.push(pattern[1])
+        }
+        return pattern
+    })
+
+    console.log(funcLines)
+    return (functions)
+}
