a702fffcdfb614921e3cc9e45db54704c14db46b
Logan CH <losesmc@gmail.com>
2017-09-23 01:23:55 -0700

    Get symbol info for variables declared on the same line

--- out/src/ai_symbols.js
+++ out/src/ai_symbols.js
@@ -4,7 +4,7 @@ var { languages, SymbolInformation, SymbolKind,
     Location, Position } = require('vscode')
 
 const _funcPattern = /Func\s(.+)\(/
-const _varPattern = /(\$\w+)/
+const _varPattern = /(\$\w+)/g
 
 module.exports = languages.registerDocumentSymbolProvider(
     { language: 'autoit', scheme: 'file' },
@@ -27,11 +27,23 @@ module.exports = languages.registerDocumentSymbolProvider(
                     '', new Location(doc.uri, new Position(line, 0))))
                     found.push(funcName[1])
                 }
-                varName = _varPattern.exec(text)
-                if (varName && found.indexOf(varName[1]) === -1) {
-                    result.push(new SymbolInformation(varName[1], SymbolKind.Variable,
-                    '', new Location(doc.uri, new Position(line, 0))))
-                    found.push(varName[1])
+
+
+                let variables = []
+                text.replace(_varPattern, (s, g1) => { // get each variable on the line
+                    variables.push(g1)
+                })
+
+                if (variables.length > 0)
+                {
+                    for (var i = 0; i < variables.length; i++)
+                    {
+                        if (found.indexOf(variables[i]) === -1) {
+                            result.push(new SymbolInformation(variables[i], SymbolKind.Variable,
+                            '', new Location(doc.uri, new Position(line, 0))))
+                            found.push(variables[i])
+                        }
+                    }
                 }
             }
 
