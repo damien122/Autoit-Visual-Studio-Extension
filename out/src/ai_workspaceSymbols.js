eff40b6cbfc74da454b12be8db394f371c3d244d
Logan Hampton <lhampton@fincoad.com>
2018-03-15 14:38:13 -0700

    Get variables to show up from entire workgroup

--- out/src/ai_workspaceSymbols.js
+++ out/src/ai_workspaceSymbols.js
@@ -1,31 +1,45 @@
 'use strict'
 
 var { languages, SymbolInformation, SymbolKind, Location, Position, 
-    workspace } = require('vscode')
+    workspace, Uri } = require('vscode')
+const fs = require('fs')
 
 const _funcPattern = /Func\s(.+)\(/
-const _varPattern = /(\$\w+)/g
+const _varPattern = /(\$\w+)\s*=/g
 var config = workspace.getConfiguration('autoit');
 
 module.exports = languages.registerWorkspaceSymbolProvider({
 
     provideWorkspaceSymbols(search, token) {
-        var files
+        var scriptText
+        var variableFound
+        var variables  = []
+        let results
 
         // Get list of AutoIt files in workspace
-        workspace.findFiles("**/*.{au3,a3x}").then((d)=> {
-            files = d
-            console.log('---')
+        return results = workspace.findFiles("**/*.{au3,a3x}").then((data) => {
             
-            console.log(files.toString())
-            console.log(files[0])
-            console.log('----')
+            for (var file in data) {
+                // Go through each file and grab functions and variables (if active)
+                scriptText = fs.readFileSync(data[file].fsPath).toString().split("\n")
+                
+                var scriptVariables = scriptText.filter((line, index) => {
+                    variableFound = _varPattern.exec(line)
+                    // Filter based on search (if it's not empty)
+                    if (variableFound) {
+                        variables.push(makeSymbol(variableFound[1], SymbolKind.Variable,
+                            data[file], index))
+                    }
+                })
+            }
 
-            // Go through each file and grab functions and variables (if active)
-            // Filter based on search (if it's not empty)
-            // Return the list in the right format
-            
+            console.log(variables)
+            return variables
         })
-        
     }
 })
+
+
+function makeSymbol(name, type, filePath, docLine) {
+    return new SymbolInformation(name, type, '', new Location(filePath, new Position(docLine, 0)))
+}
