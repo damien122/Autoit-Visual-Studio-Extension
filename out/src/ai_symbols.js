f0aaf253d94e52a8f7b33b764c23c1ac7dd42706
Logan Hampton <lhampton@fincoad.com>
2019-01-03 12:07:39 -0700

    Code cleanup

--- out/src/ai_symbols.js
+++ out/src/ai_symbols.js
@@ -15,7 +15,6 @@ module.exports = languages.registerDocumentSymbolProvider(
             let result = []
             let found = []
             let funcName
-            let varName
 
             // Get the number of lines in the document to loop through
             const lineCount = Math.min(doc.lineCount, 10000)
@@ -28,29 +27,33 @@ module.exports = languages.registerDocumentSymbolProvider(
                 }
 
                 funcName = _funcPattern.exec(text)
-                if(funcName && found.indexOf(funcName[1]) === -1) {
-                    result.push(new SymbolInformation(funcName[1], SymbolKind.Function, 
-                    '', new Location(doc.uri, new Position(lineNum, 0))))
+                if (funcName && !found.includes(funcName[1])) {
+
+                    let functionSymbol = new SymbolInformation(funcName[1], SymbolKind.Function, line.range)
+                    result.push(functionSymbol)
                     found.push(funcName[1])
                 }
 
                 if (config.showVariablesInGoToSymbol) {
-                    let variables = []
-                    text.replace(_varPattern, (s, g1) => { // get each variable on the line
-                        variables.push(g1)
+
+                    let variables = text.match(_varPattern)
+                    
+                    if (!variables) {
+                        continue
+                    }
+
+                    variables.forEach((variable) => {
+                        
+                        if (found.includes(variable) || AI_CONSTANTS.includes(variable)) {
+                            return
+                        }
+
+                        let variableSymbol = new SymbolInformation(variable, SymbolKind.Variable, line.range)
+                        result.push(variableSymbol)
+                        found.push(variable)
+
                     })
 
-                    if (variables.length > 0)
-                    {
-                        for (var i = 0; i < variables.length; i++)
-                        {
-                            if (found.indexOf(variables[i]) === -1 && AI_CONSTANTS.indexOf(variables[i]) === -1) {
-                                result.push(new SymbolInformation(variables[i], SymbolKind.Variable,
-                                '', new Location(doc.uri, new Position(lineNum, 0))))
-                                found.push(variables[i])
-                            }
-                        }
-                    }
                 }
             }
 
