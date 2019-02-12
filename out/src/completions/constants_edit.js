f937a7a6764978dc8584381a0c4310a54c84df2d
Logan Hampton <lhampton@fincoad.com>
2019-02-12 08:13:59 -0700

    Change and add items to match EditConstants.au3

--- out/src/completions/constants_edit.js
+++ out/src/completions/constants_edit.js
@@ -1,108 +1,412 @@
 var {
-    CompletionItemKind
+    CompletionItemKind,
+    MarkdownString
 } = require('vscode')
 
+const style = 'Style Constant'
+const message = 'Message Constant'
+const param = 'Param Constant'
+const status = 'Status Constant'
+const notification = 'Notification Constant'
+const control = 'Control Default Style Constant'
+
 var items = [{
-        label: '$SS_BLACKFRAME',
-        documentation: 'Specifies a box with a frame around the control that is the same color as the Window Frame. The default Windows color is black. Frame-style static controls do not display text.  ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_LEFT',
+        documentation: '`= 0`',
+        detail: style
     },
     {
-        label: '$SS_BLACKRECT',
-        documentation: 'Specifies a rectangle filled with the current window frame color. This color is black in the default color scheme. Rectangle-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_CENTER',
+        documentation: '`= 1`',
+        detail: style
     },
     {
-        label: '$SS_CENTER',
-        documentation: 'Specifies a simple rectangle and centers the error value text in the rectangle. The control automatically wraps words that extend past the end of a line to the beginning of the next centered line. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_RIGHT',
+        documentation: '`= 2`',
+        detail: style
     },
     {
-        label: '$SS_CENTERIMAGE',
-        documentation: 'Specifies that the midpoint of a static control with the SS_BITMAP style will remain fixed when you resize the control. The four sides are adjusted to accommodate a new bitmap. If the bitmap is smaller than the control\'s client area, the rest of the client area is filled with the color of the pixel in the upper-left corner of the bitmap. It can be used with static control having only one line of text.\nThis style bit do not result in unused portions of the control being filled with the color of the top left pixel of the bitmap or icon. Unused portions of the control will remain the background color. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_MULTILINE',
+        documentation: '`= 4`',
+        detail: style
     },
     {
-        label: '$SS_ETCHEDFRAME',
-        documentation: 'Draws the frame of the static control using the EDGE_ETCHED edge style. Frame-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_UPPERCASE',
+        documentation: '`= 8`',
+        detail: style
     },
     {
-        label: '$SS_ETCHEDHORZ',
-        documentation: 'Draws the top and bottom edges of the static control using the EDGE_ETCHED edge style. Frame-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_LOWERCASE',
+        documentation: '`= 16`',
+        detail: style
     },
     {
-        label: '$SS_ETCHEDVERT',
-        documentation: 'Draws the left and right edges of the static control using the EDGE_ETCHED edge style. Frame-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_PASSWORD',
+        documentation: '`= 32`',
+        detail: style
     },
     {
-        label: '$SS_GRAYFRAME',
-        documentation: 'Specifies a box with a frame drawn with the same color as the screen background (desktop). This color is gray in the default color scheme. Frame-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_AUTOVSCROLL',
+        documentation: '`= 64`',
+        detail: style
     },
     {
-        label: '$SS_GRAYRECT',
-        documentation: 'Specifies a rectangle filled with the current screen background color. This color is gray in the default color scheme. Rectangle-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_AUTOHSCROLL',
+        documentation: '`= 128`',
+        detail: style
     },
     {
-        label: '$SS_LEFT',
-        documentation: 'Specifies a simple rectangle and left-aligns the text in the rectangle. The text is formatted before it is displayed. Words that extend past the end of a line are automatically wrapped to the beginning of the next left-aligned line. Words that are longer than the width of the control are truncated. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_NOHIDESEL',
+        documentation: '`= 256`',
+        detail: style
     },
     {
-        label: '$SS_LEFTNOWORDWRAP',
-        documentation: 'Specifies a rectangle and left-aligns the text in the rectangle. Tabs are expanded, but words are not wrapped. Text that extends past the end of a line is clipped. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_OEMCONVERT',
+        documentation: '`= 1024`',
+        detail: style
     },
     {
-        label: '$SS_NOPREFIX',
-        documentation: 'Prevents interpretation of any ampersand (&) characters in the control\'s text as accelerator prefix characters.\nAn application can combine SS_NOPREFIX with other styles by using the bitwise OR (|) operator. This can be useful when file names or other strings that might contain an ampersand (&) must be displayed within a static control in a dialog box. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_READONLY',
+        documentation: '`= 2048`',
+        detail: style
     },
     {
-        label: '$SS_NOTIFY',
-        documentation: 'Sends the parent window the STN_CLICKED notification when the user clicks the control. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_WANTRETURN',
+        documentation: '`= 4096`',
+        detail: style
     },
     {
-        label: '$SS_RIGHT',
-        documentation: 'Specifies a rectangle and right-aligns the specified text in the rectangle. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ES_NUMBER',
+        documentation: '`= 8192`',
+        detail: style
     },
     {
-        label: '$SS_RIGHTJUST',
-        documentation: 'Specifies that the lower right corner of a static control with the SS_BITMAP or SS_ICON style is to remain fixed when the control is resized. Only the top and left sides are adjusted to accommodate a new bitmap or icon. ',
-        detail: 'Label/Static Style Constant'
+        label: '$EC_ERR',
+        documentation: '`= -1`',
+        detail: 'Error Checking Constant'
     },
     {
-        label: '$SS_SIMPLE',
-        documentation: 'Specifies a simple rectangle and displays a single line of left-aligned text in the rectangle. The text line cannot be shortened or altered in any way. Also, if the control is disabled, the control does not gray its text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$ECM_FIRST',
+        documentation: '`= 0X1500`',
+        detail: message
     },
     {
-        label: '$SS_SUNKEN',
-        documentation: 'Draws a half-sunken border around a static control. ',
-        detail: 'Label/Static Style Constant'
+        label: '$EM_CANUNDO',
+        documentation: '`= 0xC6`',
+        detail: message
     },
     {
-        label: '$SS_WHITEFRAME',
-        documentation: 'Specifies a box with a frame drawn with the same color as the window background. This color is white in the default color scheme. Frame-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$EM_CHARFROMPOS',
+        documentation: '`= 0xD7`',
+        detail: message
     },
     {
-        label: '$SS_WHITERECT',
-        documentation: 'Specifies a rectangle filled with the current window background color. This color is white in the default color scheme. Rectangle-style static controls do not display text. ',
-        detail: 'Label/Static Style Constant'
+        label: '$EM_EMPTYUNDOBUFFER',
+        documentation: '`= 0xCD`',
+        detail: message
+    },
+    {
+        label: '$EM_FMTLINES',
+        documentation: '`= 0xC8`',
+        detail: message
+    },
+    {
+        label: '$EM_GETCUEBANNER',
+        documentation: '`= ($ECM_FIRST + 2)`',
+        detail: message
+    },
+    {
+        label: '$EM_GETFIRSTVISIBLELINE',
+        documentation: '`= 0xCE`',
+        detail: message
+    },
+    {
+        label: '$EM_GETHANDLE',
+        documentation: '`= 0xBD`',
+        detail: message
+    },
+    {
+        label: '$EM_GETIMESTATUS',
+        documentation: '`= 0xD9`',
+        detail: message
+    },
+    {
+        label: '$EM_GETLIMITTEXT',
+        documentation: '`= 0xD5`',
+        detail: message
+    },
+    {
+        label: '$EM_GETLINE',
+        documentation: '`= 0xC4`',
+        detail: message
+    },
+    {
+        label: '$EM_GETLINECOUNT',
+        documentation: '`= 0xBA`',
+        detail: message
+    },
+    {
+        label: '$EM_GETMARGINS',
+        documentation: '`= 0xD4`',
+        detail: message
+    },
+    {
+        label: '$EM_GETMODIFY',
+        documentation: '`= 0xB8`',
+        detail: message
+    },
+    {
+        label: '$EM_GETPASSWORDCHAR',
+        documentation: '`= 0xD2`',
+        detail: message
+    },
+    {
+        label: '$EM_GETRECT',
+        documentation: '`= 0xB2`',
+        detail: message
+    },
+    {
+        label: '$EM_GETSEL',
+        documentation: '`= 0xB0`',
+        detail: message
+    },
+    {
+        label: '$EM_GETTHUMB',
+        documentation: '`= 0xBE`',
+        detail: message
+    },
+    {
+        label: '$EM_GETWORDBREAKPROC',
+        documentation: '`= 0xD1`',
+        detail: message
+    },
+    {
+        label: '$EM_HIDEBALLOONTIP',
+        documentation: '`= ($ECM_FIRST + 4)`',
+        detail: message
+    },
+    {
+        label: '$EM_LIMITTEXT',
+        documentation: '`= 0xC5`',
+        detail: message
+    },
+    {
+        label: '$EM_LINEFROMCHAR',
+        documentation: '`= 0xC9`',
+        detail: message
+    },
+    {
+        label: '$EM_LINEINDEX',
+        documentation: '`= 0xBB`',
+        detail: message
+    },
+    {
+        label: '$EM_LINELENGTH',
+        documentation: '`= 0xC1`',
+        detail: message
+    },
+    {
+        label: '$EM_LINESCROLL',
+        documentation: '`= 0xB6`',
+        detail: message
+    },
+    {
+        label: '$EM_POSFROMCHAR',
+        documentation: '`= 0xD6`',
+        detail: message
+    },
+    {
+        label: '$EM_REPLACESEL',
+        documentation: '`= 0xC2`',
+        detail: message
+    },
+    {
+        label: '$EM_SCROLL',
+        documentation: '`= 0xB5`',
+        detail: message
+    },
+    {
+        label: '$EM_SCROLLCARET',
+        documentation: '`= 0x00B7`',
+        detail: message
+    },
+    {
+        label: '$EM_SETCUEBANNER',
+        documentation: '`= ($ECM_FIRST + 1)`',
+        detail: message
+    },
+    {
+        label: '$EM_SETHANDLE',
+        documentation: '`= 0xBC`',
+        detail: message
+    },
+    {
+        label: '$EM_SETIMESTATUS',
+        documentation: '`= 0xD8`',
+        detail: message
+    },
+    {
+        label: '$EM_SETLIMITTEXT',
+        documentation: '`= $EM_LIMITTEXT`',
+        detail: message
+    },
+    {
+        label: '$EM_SETMARGINS',
+        documentation: '`= 0xD3`',
+        detail: message
+    },
+    {
+        label: '$EM_SETMODIFY',
+        documentation: '`= 0xB9`',
+        detail: message
+    },
+    {
+        label: '$EM_SETPASSWORDCHAR',
+        documentation: '`= 0xCC`',
+        detail: message
+    },
+    {
+        label: '$EM_SETREADONLY',
+        documentation: '`= 0xCF`',
+        detail: message
+    },
+    {
+        label: '$EM_SETRECT',
+        documentation: '`= 0xB3`',
+        detail: message
+    },
+    {
+        label: '$EM_SETRECTNP',
+        documentation: '`= 0xB4`',
+        detail: message
+    },
+    {
+        label: '$EM_SETSEL',
+        documentation: '`= 0xB1`',
+        detail: message
+    },
+    {
+        label: '$EM_SETTABSTOPS',
+        documentation: '`= 0xCB`',
+        detail: message
+    },
+    {
+        label: '$EM_SETWORDBREAKPROC',
+        documentation: '`= 0xD0`',
+        detail: message
+    },
+    {
+        label: '$EM_SHOWBALLOONTIP',
+        documentation: '`= ($ECM_FIRST + 3)`',
+        detail: message
+    },
+    {
+        label: '$EM_UNDO',
+        documentation: '`= 0xC7`',
+        detail: message
+    },
+    {
+        label: '$EC_LEFTMARGIN',
+        documentation: '`= 0x1`',
+        detail: param
+    },
+    {
+        label: '$EC_RIGHTMARGIN',
+        documentation: '`= 0x2`',
+        detail: param
+    },
+    {
+        label: '$EC_USEFONTINFO',
+        documentation: '`= 0xFFFF`',
+        detail: param
+    },
+    {
+        label: '$EMSIS_COMPOSITIONSTRING',
+        documentation: '`= 0x1`',
+        detail: param
+    },
+    {
+        label: '$EIMES_GETCOMPSTRATONCE',
+        documentation: 'If this flag is set, the edit control hooks the WM_IME_COMPOSITION message with fFlags set to GCS_RESULTSTR and returns the result string immediately\n\n`= 0x1 `',
+        detail: status
+    },
+    {
+        label: '$EIMES_CANCELCOMPSTRINFOCUS',
+        documentation: 'If this flag is set, the edit control cancels the composition string when it receives the WM_SETFOCUS message.\n\n`= 0x2 `',
+        detail: status
+    },
+    {
+        label: '$EIMES_COMPLETECOMPSTRKILLFOCUS',
+        documentation: 'If this flag is set, the edit control completes the composition string upon receiving the WM_KILLFOCUS message.\n\n`= 0x4`',
+        detail: status
+    },
+    {
+        label: '$EN_ALIGN_LTR_EC',
+        documentation: '`= 0x700`',
+        detail: notification
+    },
+    {
+        label: '$EN_ALIGN_RTL_EC',
+        documentation: '`= 0x701`',
+        detail: notification
+    },
+    {
+        label: '$EN_CHANGE',
+        documentation: '`= 0x300`',
+        detail: notification
+    },
+    {
+        label: '$EN_ERRSPACE',
+        documentation: '`= 0x500`',
+        detail: notification
+    },
+    {
+        label: '$EN_HSCROLL',
+        documentation: '`= 0X601`',
+        detail: notification
+    },
+    {
+        label: '$EN_KILLFOCUS',
+        documentation: '`= 0x200`',
+        detail: notification
+    },
+    {
+        label: '$EN_MAXTEXT',
+        documentation: '`= 0x501`',
+        detail: notification
+    },
+    {
+        label: '$EN_SETFOCUS',
+        documentation: '`= 0x100`',
+        detail: notification
+    },
+    {
+        label: '$EN_UPDATE',
+        documentation: '`= 0x400`',
+        detail: notification
+    },
+    {
+        label: '$EN_VSCROLL',
+        documentation: '`= 0x602`',
+        detail: notification
+    },
+    {
+        label: '$GUI_SS_DEFAULT_EDIT',
+        documentation: '`= 0x003010c0 `',
+        detail: control
+    },
+    {
+        label: '$GUI_SS_DEFAULT_INPUT',
+        documentation: '`= 0x00000080`',
+        detail: control
     },
 ]
 
 // Add the icon and detail to each entry
 for (var i of items) {
     i.kind = CompletionItemKind.Constant
-    i.documentation += '\n\nRequires #include <StaticConstants.au3>'
+    i.documentation = new MarkdownString(i.documentation)
+    i.detail += '\nRequires #include <EditConstants.au3>'
 }
 
 module.exports = items
