a69b61f56796911c78d3400729ca85b87feb38e2
Logan Hampton <losesmc@gmail.com>
2019-03-01 12:49:04 -0700

    Add pattern to prevent indent changes for comments or include lines

--- out/src/languageConfiguration.js
+++ out/src/languageConfiguration.js
@@ -2,6 +2,7 @@ const languageConfiguration = {
   indentationRules: {
     increaseIndentPattern: /^\s*(For|Func|If|ElseIf|Else|Select|While|Case|Switch|With)\b/i,
     decreaseIndentPattern: /^\s*(Next|EndFunc|EndIf|ElseIf|Else|EndSelect|EndSwitch|WEnd|EndWith)\b/i,
+    unIndentedLinePattern: /^\s*((;|#include).*)?$/
   },
   wordPattern: /([$@#]?[A-Za-z_]\w+)/,
 
