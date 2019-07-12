775845d0be5dc75896aa0607e3bc8fb1d7ec9d1c
Logan Hampton <losesmc@gmail.com>
2019-07-12 14:03:38 -0700

    Adds rework of def search & start multi-file search

--- out/src/ai_definition.js
+++ out/src/ai_definition.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const { DefinitionProvider, languages, Location } = require('vscode')
+const { DefinitionProvider, languages, Location, workspace } = require('vscode')
 const { AUTOIT_MODE, isSkippableLine } = require('./util')
-
+const fs = require('fs')
 
 
 const AutoItDefinitionProvider = {
@@ -12,30 +12,81 @@ const AutoItDefinitionProvider = {
         const lookupRange = document.getWordRangeAtPosition(position)
         const lookup = document.getText(lookupRange)
         let defRegex = new RegExp(`Func\\s${lookup}\\(`)
+        let scriptText
 
-        console.log(`Looking up definition for ${lookup}!`)
-
-        
         if (lookup.charAt(0) == '$') {
             defRegex = new RegExp(`(?:(?:Local|Global|Const)\s)?\\${lookup}\\s?=?`, 'i')
+            console.log((`Looking up definition for ${lookup}!`))
+        } else {
+            console.log(`Looking up definition for ${lookup}()!`)
         }
         
         // Search current document for definition first
-        for (let lineNum = 0; lineNum < document.lineCount; lineNum++) {
-            const line = document.lineAt(lineNum)
-            const lineText = line.text
+        // for (let lineNum = 0; lineNum < document.lineCount; lineNum++) {
+        //     const line = document.lineAt(lineNum)
+        //     const lineText = line.text
 
-            if (isSkippableLine(line)) {
-                continue
-            }
+        //     if (isSkippableLine(line)) {
+        //         continue
+        //     }
 
-            if (lineText.match(defRegex)) {
-                return new Location(document.uri, line.range)
-            }
+        //     if (lineText.match(defRegex)) {
+        //         return new Location(document.uri, line.range)
+        //     }
+        // }
+
+        let found = document.getText().match(defRegex)
+
+        if (found) {
+            return new Location(document.uri, document.positionAt(found.index))
         }
+
+
+
+        // If nothing was found, search workspace
+        // Get workspace files
+        
+        const workspaceSearch = searchWorkspaceScripts(defRegex, document)
+
+        return workspaceSearch
     }
 }
 
 const defProvider = languages.registerDefinitionProvider(AUTOIT_MODE, AutoItDefinitionProvider)
 
 module.exports = defProvider
+
+const searchWorkspaceScripts = (lookup, currentDoc) => {
+    let scriptText
+    let scriptLine
+
+    let locations = []
+
+    return workspace.findFiles("**/*.{au3,a3x}").then((allFiles) => {
+        
+        // Loop through each, running regex    
+        for (let fileIndex = 0; fileIndex < allFiles.length; fileIndex++) {
+            let found
+            // Skip originating document
+            if (allFiles[fileIndex] == currentDoc.uri) {
+                continue
+            }
+
+            workspace.openTextDocument(allFiles[fileIndex]).then((script) => {
+                const scriptContent = script.getText()
+                // skip if the lookup can't be found
+                if (lookup.test(scriptContent) == false) {
+                    return
+                }
+
+                found = scriptContent.match(lookup)
+
+                if (found) {
+                    locations.push(new Location(script.uri, script.positionAt(found.index)))
+                }
+            })
+        }
+        
+        return locations
+    })
+}
