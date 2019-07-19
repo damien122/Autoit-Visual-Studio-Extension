0f2ccf6e7bd837f8b4ed81ec17a287cddaa1aa84
Logan Hampton <losesmc@gmail.com>
2019-07-19 11:51:55 -0700

    Adds getIncludePath function

--- out/src/util.js
+++ out/src/util.js
@@ -1,7 +1,9 @@
-const fs = require('fs')
+const fs = require("fs")
+const path = require("path")
 
-const descriptionHeader = "|Description |Value |\n|:---|:---:|\n";
-const valueFirstHeader = "\n|&nbsp;|&nbsp;&nbsp;&nbsp; |&nbsp;\n|---:|:---:|:---|";
+const descriptionHeader = "|Description |Value |\n|:---|:---:|\n"
+const valueFirstHeader =
+    "\n|&nbsp;|&nbsp;&nbsp;&nbsp; |&nbsp;\n|---:|:---:|:---|"
 const trueFalseHeader = `\n|&nbsp;|&nbsp;&nbsp;&nbsp;|&nbsp;
     :---|:---:|:---`
 const opt = "##### [optional]  \n"
@@ -9,21 +11,26 @@ const br = "\u0020\u0020"
 const defaultZero = br + br + "`Default = 0`"
 
 const setDetailAndDocumentation = (array, detail, doc) => {
-    
     for (let i of array) {
-        i.detail = detail,
+        i.detail = detail
         i.documentation += `\n\n*${doc}*`
     }
-    
+
     return array
 }
 
-const AI_CONSTANTS = ['$MB_ICONERROR', '$MB_ICONINFORMATION', '$MB_YESNO', '$MB_TASKMODAL', '$IDYES', '$IDNO',]
-const AUTOIT_MODE = { language: 'autoit', scheme: 'file' }
+const AI_CONSTANTS = [
+    "$MB_ICONERROR",
+    "$MB_ICONINFORMATION",
+    "$MB_YESNO",
+    "$MB_TASKMODAL",
+    "$IDYES",
+    "$IDNO",
+]
+const AUTOIT_MODE = { language: "autoit", scheme: "file" }
 
-
-const isSkippableLine = (line) => {
-    const skipChars = [';', '#']
+const isSkippableLine = line => {
+    const skipChars = [";", "#"]
 
     if (line.isEmptyOrWhitespace) {
         return true
@@ -37,8 +44,28 @@ const isSkippableLine = (line) => {
     return false
 }
 
-const getIncludeText = (filePath) => {
+const getIncludeText = filePath => {
     return fs.readFileSync(filePath).toString()
+}
+
+const getIncludePath = (fileOrPath, document) => {
+    let includePath = ""
+
+    if (fileOrPath.charAt(1) == ":") {
+        includePath = fileOrPath
+    } else {
+        let docDir = path.dirname(document.fileName)
+
+        docDir +=
+            (fileOrPath.charAt(0) == "\\" || fileOrPath.charAt(0) == "/"
+                ? ""
+                : "\\") + fileOrPath
+        includePath = path.normalize(docDir)
+    }
+
+    includePath = includePath.charAt(0).toUpperCase() + includePath.slice(1)
+
+    return includePath
 }
 
 module.exports = {
@@ -53,4 +80,5 @@ module.exports = {
     AUTOIT_MODE,
     isSkippableLine,
     getIncludeText,
+    getIncludePath,
 }
