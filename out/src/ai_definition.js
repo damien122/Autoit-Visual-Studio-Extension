3174a8d0cbdd355736682b177b1f76e07af9b0a2
Logan Hampton <losesmc@gmail.com>
2019-07-19 11:55:49 -0700

    Simplify code to look for definitions in includes

--- out/src/ai_definition.js
+++ out/src/ai_definition.js
@@ -1,78 +1,77 @@
-'use strict'
+"use strict"
 
-const { DefinitionProvider, languages, Location, workspace } = require('vscode')
-const { AUTOIT_MODE, isSkippableLine } = require('./util')
-const fs = require('fs')
-
+const {
+    DefinitionProvider,
+    languages,
+    Location,
+    Position,
+    workspace,
+    Uri,
+} = require("vscode")
+const {
+    AUTOIT_MODE,
+    isSkippableLine,
+    getIncludePath,
+    getIncludeText,
+} = require("./util")
+const fs = require("fs")
+const path = require("path")
 
 const AutoItDefinitionProvider = {
-
     provideDefinition(document, position, token) {
-
         const lookupRange = document.getWordRangeAtPosition(position)
         const lookup = document.getText(lookupRange)
+        const docText = document.getText()
         let defRegex = new RegExp(`Func\\s${lookup}\\(`)
-        let scriptText
+        const includePattern = /^\s*#include\s"(.+)"/gm
 
-        if (lookup.charAt(0) == '$') {
-            defRegex = new RegExp(`(?:(?:Local|Global|Const)\s)?\\${lookup}\\s?=?`, 'i')
-            console.log((`Looking up definition for ${lookup}!`))
+        if (lookup.charAt(0) == "$") {
+            defRegex = new RegExp(
+                `(?:(?:Local|Global|Const)\s)?\\${lookup}\\s?=?`,
+                "i"
+            )
+            console.log(`Looking up definition for ${lookup}!`)
         } else {
             console.log(`Looking up definition for ${lookup}()!`)
         }
-        
-        let found = document.getText().match(defRegex)
+
+        let found = docText.match(defRegex)
 
         if (found) {
             return new Location(document.uri, document.positionAt(found.index))
         }
 
+        // If nothing was found, search include files
+        let scriptsToSearch = []
+        while ((found = includePattern.exec(docText))) {
+            scriptsToSearch.push(found[1])
+        }
 
+        if (Array.isArray(scriptsToSearch) && scriptsToSearch.length) {
+            let scriptFind = null
+            let found = null
+            for (let i = 0; i < scriptsToSearch.length; i++) {
+                let scriptPath = getIncludePath(scriptsToSearch[i], document)
+                let scriptContent = getIncludeText(scriptPath)
 
-        // If nothing was found, search workspace
-        // Get workspace files
-        
-        const workspaceSearch = searchWorkspaceScripts(defRegex, document)
-
-        return workspaceSearch
-    }
-}
-
-const defProvider = languages.registerDefinitionProvider(AUTOIT_MODE, AutoItDefinitionProvider)
-
-module.exports = defProvider
-
-const searchWorkspaceScripts = (lookup, currentDoc) => {
-    let scriptText
-    let scriptLine
-
-    let locations = []
-
-    return workspace.findFiles("**/*.{au3,a3x}").then((allFiles) => {
-        
-        // Loop through each, running regex    
-        for (let fileIndex = 0; fileIndex < allFiles.length; fileIndex++) {
-            let found
-            // Skip originating document
-            if (allFiles[fileIndex] == currentDoc.uri) {
-                continue
-            }
-
-            workspace.openTextDocument(allFiles[fileIndex]).then((script) => {
-                const scriptContent = script.getText()
-                // skip if the lookup can't be found
-                if (lookup.test(scriptContent) == false) {
-                    return
-                }
-
-                found = scriptContent.match(lookup)
+                found = scriptContent.match(defRegex)
 
                 if (found) {
-                    locations.push(new Location(script.uri, script.positionAt(found.index)))
+                    let line = scriptContent.slice(0, found.index).match(/\n/g).length
+
+                    return new Location(
+                        Uri.file(scriptPath),
+                        new Position(line, found.index)
+                    )
                 }
-            })
+            }
         }
-        
-        return locations
-    })
+    },
 }
+
+const defProvider = languages.registerDefinitionProvider(
+    AUTOIT_MODE,
+    AutoItDefinitionProvider
+)
+
+module.exports = defProvider
