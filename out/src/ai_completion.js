1239995233aec7470aaa900d5c851db3d2726f64
Logan Hampton <losesmc@gmail.com>
2019-07-23 12:21:58 -0700

    Begins reformat to airbnb style for completions

--- out/src/ai_completion.js
+++ out/src/ai_completion.js
@@ -1,187 +1,163 @@
-import completions from "./completions"
-var {
-    languages,
-    CompletionItem,
-    CompletionItemKind,
-    window,
-    workspace,
-} = require("vscode")
-var fs = require("fs")
-var path = require("path")
-var newComp
-var currentIncludeFiles = []
-var includes = []
-const { getIncludeText, getIncludePath } = require("./util")
+import { languages, CompletionItem, CompletionItemKind, workspace, Range } from 'vscode';
+import fs from 'fs';
+import path from 'path';
+import completions from './completions';
+import { getIncludeText, getIncludePath } from './util';
+import DEFAULT_UDFS from './constants';
 
+let currentIncludeFiles = [];
+let includes = [];
 
+const functionPattern = /Func\s+(\w*)\s*\(/g;
+const variablePattern = /\$(\w*)/g;
+const includePattern = /^\s+#include\s"(.+)"/gm;
+const LIBRARY_INCLUDE_PATTERN = /^#include\s+<([\w.]+\.au3)>/gm;
 
-const _funcPattern = /Func\s+(\w*)\s*\(/g
-const _varPattern = /\$(\w*)/g
-const _includePattern = /^\s+#include\s"(.+)"/gm
-const LIBRARY_INCLUDE_PATTERN = /^#include\s+<([\w.]+\.au3)>/gm
-const { DEFAULT_UDFS } = require("./constants")
+const createNewCompletionItem = (kind, name, strDetail = 'Document Function') => {
+  const compItem = new CompletionItem(name, kind);
+
+  compItem.detail = kind === CompletionItemKind.Variable ? 'Variable' : strDetail;
+
+  return compItem;
+};
+
+const arraysMatch = (arr1, arr2) => {
+  if (arr1.length === arr2.length && arr1.some(v => arr2.indexOf(v) <= 0)) {
+    return true;
+  }
+  return false;
+};
+
+const provideCompletionItems = (document, position) => {
+  // Gather the functions created by the user
+  const added = {};
+  let result = [];
+  const text = document.getText();
+  let range = document.getWordRangeAtPosition(position);
+  const prefix = range ? document.getText(range) : '';
+  const includesCheck = [];
+  const libraryIncludes = [];
+
+  if (!range) {
+    range = new Range(position, position);
+  }
+
+  if (prefix[0] === '$') {
+    let pattern = variablePattern.exec(text);
+    while (pattern) {
+      const varName = pattern[0];
+      if (!added[varName]) {
+        added[varName] = true;
+        result.push(createNewCompletionItem(CompletionItemKind.Variable, varName));
+      }
+
+      pattern = variablePattern.exec(text);
+    }
+  }
+
+  let pattern = functionPattern.exec(text);
+  while (pattern) {
+    const funcName = pattern[1];
+    if (!added[funcName]) {
+      added[funcName] = true;
+      result.push(createNewCompletionItem(CompletionItemKind.Function, funcName));
+    }
+    pattern = functionPattern.exec(text);
+  }
+
+  // collect the includes of the document
+  pattern = includePattern.exec(text);
+  while (pattern) {
+    includesCheck.push(pattern[1]);
+    pattern = includePattern.exec(text);
+  }
+
+  // Redo the include collecting if the includes are different
+  if (!arraysMatch(includesCheck, currentIncludeFiles)) {
+    includes = [];
+    let includeFunctions = [];
+    for (const i in includesCheck) {
+      includeFunctions = getIncludeData(includesCheck[i], document);
+      if (includeFunctions) {
+        for (var newFunc in includeFunctions) {
+          includes.push(
+            createNewCompletionItem(
+              CompletionItemKind.Function,
+              includeFunctions[newFunc],
+              `Function from ${includesCheck[i]}`,
+            ),
+          );
+        }
+      }
+    }
+    currentIncludeFiles = includesCheck;
+  }
+
+  // Collect the library includes
+  while ((pattern = LIBRARY_INCLUDE_PATTERN.exec(text))) {
+    // Filter out the default UDFs
+    const filename = pattern[1].replace('.au3', '');
+    if (DEFAULT_UDFS.indexOf(filename) == -1) {
+      libraryIncludes.push(pattern[1]);
+    }
+  }
+
+  const library = [];
+  for (const file of libraryIncludes) {
+    const fullPath = findFilepath(file);
+    if (fullPath) {
+      const libraryResults = getIncludeData(fullPath, document);
+      if (libraryResults) {
+        for (var newFunc in libraryResults) {
+          library.push(
+            createNewCompletionItem(
+              CompletionItemKind.Function,
+              libraryResults[newFunc],
+              `Function from ${file}`,
+            ),
+          );
+        }
+      }
+    }
+  }
+
+  result = result.concat(includes, library); // Add either the existing include functions or the new ones to result
+
+  return completions.concat(result);
+};
 
 module.exports = languages.registerCompletionItemProvider(
-    { language: "autoit", scheme: "file" },
-    {
-        provideCompletionItems(document, position, token) {
-            // Gather the functions created by the user
-            var added = {}
-            var result = []
-            var text = document.getText()
-            var range = document.getWordRangeAtPosition(position)
-            var prefix = range ? document.getText(range) : ""
-            var includesCheck = []
-            var libraryIncludes = []
-
-            if (!range) {
-                range = new Range(position, position)
-            }
-
-            var createNewCompletionItem = function(
-                kind,
-                name,
-                strDetail = "Document Function"
-            ) {
-                var compItem = new CompletionItem(name, kind)
-                if (kind == CompletionItemKind.Variable) {
-                    strDetail = "Variable"
-                }
-                compItem.detail = strDetail
-
-                return compItem
-            }
-
-            if (prefix[0] === "$") {
-                var pattern = null
-                while ((pattern = _varPattern.exec(text))) {
-                    var varName = pattern[0]
-                    if (!added[varName]) {
-                        added[varName] = true
-                        result.push(
-                            createNewCompletionItem(
-                                CompletionItemKind.Variable,
-                                varName
-                            )
-                        )
-                    }
-                }
-            }
-
-            var pattern = null
-            while ((pattern = _funcPattern.exec(text))) {
-                var funcName = pattern[1]
-                if (!added[funcName]) {
-                    added[funcName] = true
-                    result.push(
-                        createNewCompletionItem(
-                            CompletionItemKind.Function,
-                            funcName
-                        )
-                    )
-                }
-            }
-
-            // collect the includes of the document
-            var pattern = null
-            while ((pattern = _includePattern.exec(text))) {
-                includesCheck.push(pattern[1])
-            }
-
-            // Redo the include collecting if the includes are different
-            if (!arraysMatch(includesCheck, currentIncludeFiles)) {
-                includes = []
-                var includeFunctions = []
-                for (var i in includesCheck) {
-                    includeFunctions = getIncludeData(
-                        includesCheck[i],
-                        document
-                    )
-                    if (includeFunctions) {
-                        for (var newFunc in includeFunctions) {
-                            includes.push(
-                                createNewCompletionItem(
-                                    CompletionItemKind.Function,
-                                    includeFunctions[newFunc],
-                                    "Function from " + includesCheck[i]
-                                )
-                            )
-                        }
-                    }
-                }
-                currentIncludeFiles = includesCheck
-            }
-
-            // Collect the library includes
-            while ((pattern = LIBRARY_INCLUDE_PATTERN.exec(text))) {
-                // Filter out the default UDFs
-                let filename = pattern[1].replace(".au3", "")
-                if (DEFAULT_UDFS.indexOf(filename) == -1) {
-                    libraryIncludes.push(pattern[1])
-                }
-            }
-
-            let library = []
-            for (let file of libraryIncludes) {
-                let fullPath = findFilepath(file)
-                if (fullPath) {
-                    let libraryResults = getIncludeData(fullPath, document)
-                    if (libraryResults) {
-                        for (var newFunc in libraryResults) {
-                            library.push(
-                                createNewCompletionItem(
-                                    CompletionItemKind.Function,
-                                    libraryResults[newFunc],
-                                    "Function from " + file
-                                )
-                            )
-                        }
-                    }
-                }
-            }
-
-            result = result.concat(includes, library) //Add either the existing include functions or the new ones to result
-
-            return completions.concat(result)
-        },
-    },
-    ".",
-    "$"
-)
+  { language: 'autoit', scheme: 'file' },
+  { provideCompletionItems },
+  '.',
+  '$',
+);
 
 function getIncludeData(fileName, document) {
-    const _includeFuncPattern = /^(?=\S)(?!;~\s)Func\s+(\w+)\s*\(/gm
-    var functions = []
-    var filePath = getIncludePath(fileName, document)
+  const _includeFuncPattern = /^(?=\S)(?!;~\s)Func\s+(\w+)\s*\(/gm;
+  const functions = [];
+  const filePath = getIncludePath(fileName, document);
 
-    var pattern = null
-    var fileData = getIncludeText(filePath)
+  let pattern = null;
+  const fileData = getIncludeText(filePath);
 
-    while ((pattern = _includeFuncPattern.exec(fileData))) {
-        functions.push(pattern[1])
-    }
+  while ((pattern = _includeFuncPattern.exec(fileData))) {
+    functions.push(pattern[1]);
+  }
 
-    return functions
+  return functions;
 }
 
 function findFilepath(file) {
-    let includePaths = workspace.getConfiguration("autoit").includePaths
+  const includePaths = workspace.getConfiguration('autoit').includePaths;
 
-    for (const iPath of includePaths) {
-        let newPath = path.normalize(iPath + "\\") + file
+  for (const iPath of includePaths) {
+    const newPath = path.normalize(`${iPath}\\`) + file;
 
-        if (fs.existsSync(newPath)) {
-            return newPath
-        }
+    if (fs.existsSync(newPath)) {
+      return newPath;
     }
+  }
 
-    return 0
-}
-
-function arraysMatch(arr1, arr2) {
-    if (arr1.length == arr2.length && arr1.some(v => arr2.indexOf(v) <= 0)) {
-        return true
-    } else {
-        return false
-    }
+  return 0;
 }
