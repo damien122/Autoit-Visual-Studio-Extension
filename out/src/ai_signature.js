aca8a73f8feaaba16bed079225caa0f28389f18e
Logan Hampton <losesmc@gmail.com>
2019-08-01 13:53:11 -0700

    Changes for airbnb style

--- out/src/ai_signature.js
+++ out/src/ai_signature.js
@@ -1,235 +1,224 @@
-"use strict"
+import {
+  languages,
+  SignatureHelp,
+  SignatureInformation,
+  ParameterInformation,
+  MarkdownString,
+  workspace,
+} from 'vscode';
+import fs from 'fs';
+import path from 'path';
+import { getIncludeText, getIncludePath } from './util';
+import defaultSigs from './signatures';
+import DEFAULT_UDFS from './constants';
 
-var {
-    languages,
-    SignatureHelp,
-    SignatureInformation,
-    ParameterInformation,
-    MarkdownString,
-    workspace,
-} = require("vscode")
-var fs = require("fs")
-var path = require("path")
-var mainFunctions = require("./signatures/functions.json")
-var udfs = require("./signatures/udfs.json")
-const { getIncludeText, getIncludePath } = require("./util")
+let currentIncludeFiles = [];
+let includes = {};
 
-const defaultSigs = Object.assign(
-    {},
-    mainFunctions,
-    udfs,
-    require("./signatures/udf_word"),
-    require("./signatures/udf_winnet"),
-    require("./signatures/udf_array")
-)
-
-var currentIncludeFiles = []
-var includes = {}
-const { DEFAULT_UDFS } = require("./constants")
-
-module.exports = languages.registerSignatureHelpProvider(
-    { language: "autoit", scheme: "file" },
-    {
-        provideSignatureHelp(document, position) {
-            // Find out what called for sig
-            let caller = getCallInfo(document, position)
-            if (caller == null) {
-                return null
-            }
-
-            //Integrate user functions
-            const signatures = Object.assign(
-                {},
-                defaultSigs,
-                getIncludes(document),
-                getLocalSigs(document)
-            )
-
-            //Get the called word from the json files
-            let foundSig = signatures[caller.func]
-            if (foundSig == null) {
-                return null
-            }
-
-            let thisSignature = new SignatureInformation(
-                foundSig.label,
-                new MarkdownString("##### " + foundSig.documentation)
-            )
-            //Enter parameter information into signature information
-            foundSig.params.forEach(element => {
-                thisSignature.parameters.push(
-                    new ParameterInformation(
-                        element.label,
-                        new MarkdownString(element.documentation)
-                    )
-                )
-            })
-
-            //Place signature information into results
-            let result = new SignatureHelp()
-            result.signatures = [thisSignature]
-            result.activeSignature = 0
-            result.activeParameter = caller.commas
-
-            return result
-        },
-    },
-    "(",
-    ","
-)
-
-function getCallInfo(doc, pos) {
-    // Acquire the text up the point where the current cursor/paren/comma is at
-    let codeAtPosition = doc.lineAt(pos.line).text.substring(0, pos.character)
-    let cleanCode = getParsableCode(codeAtPosition)
-
-    return {
-        func: getCurrentFunction(cleanCode),
-        commas: countCommas(cleanCode),
-    }
-}
-
+/**
+ * Reduces a partial line of code to the current Function for parsing
+ * @param {string} code The line of code
+ */
 function getParsableCode(code) {
-    // Remove whole inner functions from the string for easier parsing
-    code = code
-        .replace(/\w+\([^()]*\)/g, "")
-        .replace(/"[^"]*"/g, "")
-        .replace(/'[^']*'/g, "") // Remove double/single quote sets
-        .replace(/"[^"]*(?=$)/g, "") // Remove double quote and text at end of line
-        .replace(/'[^']*(?=$)/g, "") // Remove single quote and text at end of line
-        .replace(/\([^()]*\)/g, "") // Remove paren sets
-        .replace(/\({2,}/g, "(") // Reduce multiple open parens
+  const reducedCode = code
+    .replace(/\w+\([^()]*\)/g, '')
+    .replace(/"[^"]*"/g, '')
+    .replace(/'[^']*'/g, '') // Remove double/single quote sets
+    .replace(/"[^"]*(?=$)/g, '') // Remove double quote and text at end of line
+    .replace(/'[^']*(?=$)/g, '') // Remove single quote and text at end of line
+    .replace(/\([^()]*\)/g, '') // Remove paren sets
+    .replace(/\({2,}/g, '('); // Reduce multiple open parens
 
-    return code
+  return reducedCode;
 }
 
 function getCurrentFunction(code) {
-    let parenSplit = code.split("(")
-    // Get the 2nd to last item (right in front of last open paren)
-    // and clean up the results
-    return parenSplit[parenSplit.length - 2].match(/(.*)\b(\w+)/)[2]
+  const parenSplit = code.split('(');
+  // Get the 2nd to last item (right in front of last open paren)
+  // and clean up the results
+  return parenSplit[parenSplit.length - 2].match(/(.*)\b(\w+)/)[2];
 }
 
 function countCommas(code) {
-    // Find the position of the closest/last open paren
-    let openParen = code.lastIndexOf("(")
-    // Count non-string commas in text following open paren
-    let commas = code
-        .slice(openParen)
-        .match(/(?!\B["'][^"']*),(?![^"']*['"]\B)/g)
-    if (commas === null) {
-        commas = 0
-    } else {
-        commas = commas.length
-    }
+  // Find the position of the closest/last open paren
+  const openParen = code.lastIndexOf('(');
+  // Count non-string commas in text following open paren
+  let commas = code.slice(openParen).match(/(?!\B["'][^"']*),(?![^"']*['"]\B)/g);
+  if (commas === null) {
+    commas = 0;
+  } else {
+    commas = commas.length;
+  }
 
-    return commas
+  return commas;
 }
 
-function getIncludes(doc) {
-    // determines whether includes should be re-parsed or not.
-    var text = doc.getText()
-    var pattern = null
-    const includePattern = /^\s+#include\s"(.+)"/gm
-    const LIBRARY_INCLUDE_PATTERN = /^#include\s+<([\w.]+\.au3)>/gm
-    var includesCheck = []
+function getCallInfo(doc, pos) {
+  // Acquire the text up the point where the current cursor/paren/comma is at
+  const codeAtPosition = doc.lineAt(pos.line).text.substring(0, pos.character);
+  const cleanCode = getParsableCode(codeAtPosition);
 
-    while ((pattern = includePattern.exec(text))) {
-        includesCheck.push(pattern[1])
-    }
-
-    if (!arraysMatch(includesCheck, currentIncludeFiles)) {
-        includes = {}
-        for (var i in includesCheck) {
-            var newIncludes = getIncludeData(includesCheck[i], doc)
-            Object.assign(includes, newIncludes)
-        }
-        currentIncludeFiles = includesCheck
-    }
-
-    includesCheck = []
-    while ((pattern = LIBRARY_INCLUDE_PATTERN.exec(text))) {
-        let filename = pattern[1].replace(".au3", "")
-        if (DEFAULT_UDFS.indexOf(filename) == -1) {
-            let fullPath = findFilepath(pattern[1])
-            if (fullPath) {
-                let newData = getIncludeData(fullPath, doc)
-                Object.assign(includes, newData)
-            }
-        }
-    }
-
-    return includes
-}
-
-function getIncludeData(fileName, doc) {
-    // console.log(fileName)
-    const _includeFuncPattern = /(?=\S)(?!;~\s)Func\s+((\w+)\((.+)\))/g
-    var functions = {}
-    var filePath = getIncludePath(fileName, doc)
-
-    var pattern = null
-    var fileData = getIncludeText(filePath)
-
-    while ((pattern = _includeFuncPattern.exec(fileData)) !== null) {
-        functions[pattern[2]] = {
-            label: pattern[1],
-            documentation: `Function from ${fileName}`,
-            params: getParams(pattern[3]),
-        }
-    }
-
-    return functions
-}
-
-function getLocalSigs(doc) {
-    const _includeFuncPattern = /(?=\S)(?!;~\s)^Func\s+((\w+)\((.+)\))/gm
-    let text = doc.getText()
-    let functions = {}
-
-    let pattern = null
-    while ((pattern = _includeFuncPattern.exec(text)) !== null) {
-        functions[pattern[2]] = {
-            label: pattern[1],
-            documentation: "Local Function",
-            params: getParams(pattern[3]),
-        }
-    }
-
-    return functions
-}
-
-function getParams(paramText) {
-    var params = paramText.split(",")
-
-    for (var p in params) {
-        params[p] = {
-            label: params[p].trim(),
-            documentation: "",
-        }
-    }
-
-    return params
+  return {
+    func: getCurrentFunction(cleanCode),
+    commas: countCommas(cleanCode),
+  };
 }
 
 function arraysMatch(arr1, arr2) {
-    if (arr1.length == arr2.length && arr1.some(v => arr2.indexOf(v) <= 0)) {
-        return true
-    } else {
-        return false
+  if (arr1.length === arr2.length && arr1.some(v => arr2.indexOf(v) <= 0)) {
+    return true;
+  }
+  return false;
+}
+
+function getIncludes(doc) {
+  // determines whether includes should be re-parsed or not.
+  const text = doc.getText();
+
+  const includePattern = /^\s+#include\s"(.+)"/gm;
+  const LIBRARY_INCLUDE_PATTERN = /^#include\s+<([\w.]+\.au3)>/gm;
+
+  let includesCheck = [];
+  let pattern;
+  do {
+    pattern = includePattern.exec(text);
+    if (pattern) {
+      includesCheck.push(pattern[1]);
     }
+  } while (pattern);
+
+  if (!arraysMatch(includesCheck, currentIncludeFiles)) {
+    includes = {};
+    for (const i in includesCheck) {
+      const newIncludes = getIncludeData(includesCheck[i], doc);
+      Object.assign(includes, newIncludes);
+    }
+    currentIncludeFiles = includesCheck;
+  }
+
+  includesCheck = [];
+  while ((pattern = LIBRARY_INCLUDE_PATTERN.exec(text))) {
+    const filename = pattern[1].replace('.au3', '');
+    if (DEFAULT_UDFS.indexOf(filename) == -1) {
+      const fullPath = findFilepath(pattern[1]);
+      if (fullPath) {
+        const newData = getIncludeData(fullPath, doc);
+        Object.assign(includes, newData);
+      }
+    }
+  }
+
+  return includes;
+}
+
+function getLocalSigs(doc) {
+  const _includeFuncPattern = /(?=\S)(?!;~\s)^Func\s+((\w+)\((.+)\))/gm;
+  const text = doc.getText();
+  const functions = {};
+
+  let pattern = null;
+  while ((pattern = _includeFuncPattern.exec(text)) !== null) {
+    functions[pattern[2]] = {
+      label: pattern[1],
+      documentation: 'Local Function',
+      params: getParams(pattern[3]),
+    };
+  }
+
+  return functions;
+}
+
+module.exports = languages.registerSignatureHelpProvider(
+  { language: 'autoit', scheme: 'file' },
+  {
+    provideSignatureHelp(document, position) {
+      // Find out what called for sig
+      const caller = getCallInfo(document, position);
+      if (caller == null) {
+        return null;
+      }
+
+      // Integrate user functions
+      const signatures = Object.assign(
+        {},
+        defaultSigs,
+        getIncludes(document),
+        getLocalSigs(document),
+      );
+
+      // Get the called word from the json files
+      const foundSig = signatures[caller.func];
+      if (foundSig == null) {
+        return null;
+      }
+
+      const thisSignature = new SignatureInformation(
+        foundSig.label,
+        new MarkdownString(`##### ${foundSig.documentation}`),
+      );
+      // Enter parameter information into signature information
+      foundSig.params.forEach(element => {
+        thisSignature.parameters.push(
+          new ParameterInformation(element.label, new MarkdownString(element.documentation)),
+        );
+      });
+
+      // Place signature information into results
+      const result = new SignatureHelp();
+      result.signatures = [thisSignature];
+      result.activeSignature = 0;
+      result.activeParameter = caller.commas;
+
+      return result;
+    },
+  },
+  '(',
+  ',',
+);
+
+function getIncludeData(fileName, doc) {
+  // console.log(fileName)
+  const _includeFuncPattern = /(?=\S)(?!;~\s)Func\s+((\w+)\((.+)\))/g;
+  const functions = {};
+  const filePath = getIncludePath(fileName, doc);
+
+  let pattern = null;
+  const fileData = getIncludeText(filePath);
+
+  while ((pattern = _includeFuncPattern.exec(fileData)) !== null) {
+    functions[pattern[2]] = {
+      label: pattern[1],
+      documentation: `Function from ${fileName}`,
+      params: getParams(pattern[3]),
+    };
+  }
+
+  return functions;
+}
+
+function getParams(paramText) {
+  const params = paramText.split(',');
+
+  for (const p in params) {
+    params[p] = {
+      label: params[p].trim(),
+      documentation: '',
+    };
+  }
+
+  return params;
 }
 
 function findFilepath(file) {
-    let includePaths = workspace.getConfiguration("autoit").includePaths
+  const includePaths = workspace.getConfiguration('autoit').includePaths;
 
-    for (const iPath of includePaths) {
-        let newPath = path.normalize(iPath + "\\") + file
+  for (const iPath of includePaths) {
+    const newPath = path.normalize(`${iPath}\\`) + file;
 
-        if (fs.existsSync(newPath)) {
-            return newPath
-        }
+    if (fs.existsSync(newPath)) {
+      return newPath;
     }
+  }
 
-    return 0
+  return 0;
 }
