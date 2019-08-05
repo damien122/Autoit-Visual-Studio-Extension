d586d05b13a9e116c7a46a4026cd462dc3e97281
Logan Hampton <losesmc@gmail.com>
2019-08-05 12:47:28 -0700

    Format symbols.js to airbnb style

--- out/src/ai_symbols.js
+++ out/src/ai_symbols.js
@@ -1,34 +1,27 @@
-const {
-  languages,
-  SymbolInformation,
-  SymbolKind,
-  Location,
-  Position,
-  workspace,
-} = require('vscode');
-const { AI_CONSTANTS, AUTOIT_MODE } = require('./util');
+const { languages, SymbolInformation, SymbolKind, workspace } = require('vscode');
+const { AI_CONSTANTS, AUTOIT_MODE, isSkippableLine } = require('./util');
 
-const _funcPattern = /Func\s(.+)\(/;
-const _varPattern = /(\$\w+)/g;
+const functionPattern = /Func\s(.+)\(/;
+const variablePattern = /(\$\w+)/g;
 const config = workspace.getConfiguration('autoit');
 
 module.exports = languages.registerDocumentSymbolProvider(AUTOIT_MODE, {
-  provideDocumentSymbols(doc, token) {
+  provideDocumentSymbols(doc) {
     const result = [];
     const found = [];
     let funcName;
 
     // Get the number of lines in the document to loop through
     const lineCount = Math.min(doc.lineCount, 10000);
-    for (let lineNum = 0; lineNum < lineCount; lineNum++) {
+    for (let lineNum = 0; lineNum < lineCount; lineNum += 1) {
       const line = doc.lineAt(lineNum);
-      const text = line.text;
+      const { text } = line;
 
       if (isSkippableLine(line)) {
         continue;
       }
 
-      funcName = _funcPattern.exec(text);
+      funcName = functionPattern.exec(text);
       if (funcName && !found.includes(funcName[1])) {
         const functionSymbol = new SymbolInformation(funcName[1], SymbolKind.Function, line.range);
         result.push(functionSymbol);
@@ -36,7 +29,7 @@ module.exports = languages.registerDocumentSymbolProvider(AUTOIT_MODE, {
       }
 
       if (config.showVariablesInGoToSymbol) {
-        const variables = text.match(_varPattern);
+        const variables = text.match(variablePattern);
 
         if (!variables) {
           continue;
@@ -57,18 +50,3 @@ module.exports = languages.registerDocumentSymbolProvider(AUTOIT_MODE, {
     return result;
   },
 });
-
-const isSkippableLine = line => {
-  const skipChars = [';', '#'];
-
-  if (line.isEmptyOrWhitespace) {
-    return true;
-  }
-
-  const firstChar = line.text.charAt(line.firstNonWhitespaceCharacterIndex);
-  if (skipChars.includes(firstChar)) {
-    return true;
-  }
-
-  return false;
-};
