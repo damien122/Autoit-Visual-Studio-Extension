684073e58b193a97fefb21ffcdddaad1ffb19962
Logan Hampton <losesmc@gmail.com>
2019-10-23 19:08:00 -0700

    Moves the rest of Debug.au3 sigs and hovers

--- src/signatures/udf_debug.js
+++ src/signatures/udf_debug.js
@@ -86,7 +86,109 @@ const signatures = {
       },
       {
         label: '$bExit',
-        documentation: '${opt} `True` if the script must be terminated',
+        documentation: `${opt} \`True\` if the script must be terminated`,
+      },
+    ],
+  },
+  _DebugOut: {
+    documentation: `Displays output on a debugging session started by \`_DebugSetup()\` ${include}`,
+    label: '_DebugOut ( $sOutput )',
+    params: [
+      {
+        label: '$sOutput',
+        documentation:
+          'The string (or other printable value) to be output to the debugging session.',
+      },
+    ],
+  },
+  _DebugReport: {
+    documentation: `Writes to a debugging session ${include}`,
+    label: `_DebugReport ( $sData, [$bLastError] = False, [$bExit] = False )`,
+    params: [
+      {
+        label: '$sData',
+        documentation: 'Data to be reported',
+      },
+      {
+        label: '$bLastError',
+        documentation: `${opt} \`True\` if \`_WinAPI_GetLastErrorMessage()\` must be append to the data being reported`,
+      },
+      {
+        label: '$bExit',
+        documentation: `${opt} \`True\` if the script must be terminated`,
+      },
+    ],
+  },
+  _DebugReportEx: {
+    documentation: `Writes to a debugging session a formatted message ${include}`,
+    label: '_DebugReportEx ( $sData, [$bLastError] = False, [$bExit] = False )',
+    params: [
+      {
+        label: '$sData',
+        documentation: 'Data to be reported (e.g. "dllname|functionname")',
+      },
+      {
+        label: '$bLastError',
+        documentation: `${opt} \`True\` if \`_WinAPI_GetLastErrorMessage()\` must be append to the data being reported`,
+      },
+      {
+        label: '$bExit',
+        documentation: `${opt} \`True\` if the script must be terminated`,
+      },
+    ],
+  },
+  _DebugReportVar: {
+    documentation: `Writes to debugging session the content of a variable ${include}`,
+    label:
+      '_DebugReportVar ( $sVarName, $vVar, [$bErrExt] = False, [$iDebugLineNumber] = @ScriptLineNumber )',
+    params: [
+      {
+        label: '$sVarName',
+        documentation: 'string representing name of the variable or a comment',
+      },
+      {
+        label: '$vVar',
+        documentation: 'the variable to be reported',
+      },
+      {
+        label: '$bErrExt',
+        documentation: `${opt} \`True\` if \`@error\` and \`@extended\` must also be displayed`,
+      },
+      {
+        label: '$iDebugLineNumber',
+        documentation: `${opt} can be used to overwrite the current calling line number. Default = \`@ScriptLineNumber\``,
+      },
+    ],
+  },
+  _DebugSetup: {
+    documentation: `Setup up a debug session using a specific reporting type ${include}`,
+    label:
+      '_DebugSetup ( [$sTitle] = Default, [$bBugReportInfos] = False, [$vReportType] = 1, [$sLogFile] = "", [$bTimeStamp] = False )',
+    params: [
+      {
+        label: '$sTitle',
+        documentation: `${opt} Title to be displayed on the report window. Default value is "AutoIt Debug Report".`,
+      },
+      {
+        label: '$bBugReportInfos',
+        documentation: `${opt} Display BugReport infos. Default value is \`False\`.`,
+      },
+      {
+        label: '$vReportType',
+        documentation: `${opt} 1 - Report Log Window (Default).${br}
+          2 - ConsoleWrite.${br}
+          3 - MsgBox.${br}
+          4 - FileWrite into \`$sLogFile\` defines the filename.${br}
+          5 - Report Notepad Window.${br}
+          string - name of specific report function to be used.`,
+      },
+      {
+        label: '$sLogFile',
+        documentation: `${opt} Name of the file if \`$vReportType\` = \`4\``,
+      },
+      {
+        label: '$bTimeStamp',
+        documentation: `${opt} \`True\` if every message must be prefixed with a timeStamp "YYYY/MM/DD HH:MM:SS"`,
       },
     ],
   },
