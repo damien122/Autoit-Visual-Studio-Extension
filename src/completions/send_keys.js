f6f5aa436b52a0682d6c0a83e5d3419ac7cc7fac
Logan Hampton <losesmc@gmail.com>
2022-05-09 16:44:50 -0700

    refactor: switch to using fillCompletions function

--- src/completions/send_keys.js
+++ src/completions/send_keys.js
@@ -1,277 +1,367 @@
-'use strict'
+/* eslint-disable no-template-curly-in-string */
+import { CompletionItemKind, SnippetString } from 'vscode';
+import { fillCompletions } from '../util';
 
-var {
-    CompletionItemKind,
-    SnippetString
-} = require('vscode')
-
-var items = [{
+const items = [
+  {
     label: '{ALT}',
-    insertText: '{ALT}'
-}, {
+    insertText: '{ALT}',
+  },
+  {
     label: '{ALTDOWN}',
     insertText: '{ALTDOWN}',
-    documentation: 'Holds the ALT key down until {ALTUP} is sent'
-}, {
+    documentation: 'Holds the ALT key down until {ALTUP} is sent',
+  },
+  {
     label: '{ALTUP}',
     insertText: '{ALTUP}',
-    documentation: 'Releases the ALT key'
-}, {
+    documentation: 'Releases the ALT key',
+  },
+  {
     label: '{APPSKEY}',
-    documentation: 'Windows App key'
-}, {
+    documentation: 'Windows App key',
+  },
+  {
     label: '{ASC}',
     documentation: 'Send the ALT+nnnn key combination',
-    insertText: new SnippetString('{ASC ${1:0000}}')
-}, {
-    label: '{BACKSPACE}'
-}, {
+    insertText: new SnippetString('{ASC ').appendPlaceholder('0000').appendText('}'),
+  },
+  {
+    label: '{BACKSPACE}',
+  },
+  {
     label: '{BREAK}',
-    documentation: 'for Ctrl+Break processing'
-}, {
+    documentation: 'for Ctrl+Break processing',
+  },
+  {
     label: '{BROWSER_BACK}',
-    documentation: 'Select the browser "back" button'
-}, {
+    documentation: 'Select the browser "back" button',
+  },
+  {
     label: '{BROWSER_FAVORITES}',
-    documentation: 'Select the browser "favorites" button'
-}, {
+    documentation: 'Select the browser "favorites" button',
+  },
+  {
     label: '{BROWSER_FORWARD}',
-    documentation: 'Select the browser "forward" button'
-}, {
+    documentation: 'Select the browser "forward" button',
+  },
+  {
     label: '{BROWSER_HOME}',
-    documentation: 'Launch the browser and go to the home page'
-}, {
+    documentation: 'Launch the browser and go to the home page',
+  },
+  {
     label: '{BROWSER_REFRESH}',
-    documentation: 'Select the browser "refresh" button'
-}, {
+    documentation: 'Select the browser "refresh" button',
+  },
+  {
     label: '{BROWSER_SEARCH}',
-    documentation: 'Select the browser "search" button'
-}, {
+    documentation: 'Select the browser "search" button',
+  },
+  {
     label: '{BROWSER_STOP}',
-    documentation: 'Select the browser "stop" button'
-}, {
+    documentation: 'Select the browser "stop" button',
+  },
+  {
     label: '{BROWSER_STOP}',
-    documentation: 'Select the browser "stop" button'
-}, {
+    documentation: 'Select the browser "stop" button',
+  },
+  {
     label: '{CAPSLOCK}',
     documentation: 'CAPSLOCK (on/off/toggle)',
-    insertText: new SnippetString('{CAPSLOCK ${1:on\/off\/toggle}}')
-}, {
+    insertText: new SnippetString('{CAPSLOCK ')
+      .appendChoice(['on', 'off', 'toggle'])
+      .appendText('}'),
+  },
+  {
     label: '{CTRLDOWN}',
-    documentation: 'Holds the CTRL key down until {CTRLUP} is sent'
-}, {
+    documentation: 'Holds the CTRL key down until {CTRLUP} is sent',
+  },
+  {
     label: '{CTRLUP}',
-    documentation: 'Releases the CTRL key'
-}, {
+    documentation: 'Releases the CTRL key',
+  },
+  {
     label: '{DELETE}',
-}, {
+  },
+  {
     label: '{DOWN}',
-    documentation: 'Down arrow'
-}, {
-    label: '{END}'
-}, {
+    documentation: 'Down arrow',
+  },
+  {
+    label: '{END}',
+  },
+  {
     label: '{ENTER}',
-    documentation: 'ENTER key on the main keyboard'
-}, {
-    label: '{ESCAPE}'
-}, {
-    label: '{F1}'
-}, {
-    label: '{F2}'
-}, {
-    label: '{F3}'
-}, {
-    label: '{F4}'
-}, {
-    label: '{F5}'
-}, {
-    label: '{F6}'
-}, {
-    label: '{F7}'
-}, {
-    label: '{F8}'
-}, {
-    label: '{F9}'
-}, {
-    label: '{F10}'
-}, {
-    label: '{F11}'
-}, {
-    label: '{F12}'
-}, {
-    label: '{HOME}'
-}, {
-    label: '{INSERT}'
-}, {
+    documentation: 'ENTER key on the main keyboard',
+  },
+  {
+    label: '{ESCAPE}',
+  },
+  {
+    label: '{F1}',
+  },
+  {
+    label: '{F2}',
+  },
+  {
+    label: '{F3}',
+  },
+  {
+    label: '{F4}',
+  },
+  {
+    label: '{F5}',
+  },
+  {
+    label: '{F6}',
+  },
+  {
+    label: '{F7}',
+  },
+  {
+    label: '{F8}',
+  },
+  {
+    label: '{F9}',
+  },
+  {
+    label: '{F10}',
+  },
+  {
+    label: '{F11}',
+  },
+  {
+    label: '{F12}',
+  },
+  {
+    label: '{HOME}',
+  },
+  {
+    label: '{INSERT}',
+  },
+  {
     label: '{LALT}',
-    documentation: 'Left ALT key'
-}, {
+    documentation: 'Left ALT key',
+  },
+  {
     label: '{LAUNCH_APP1}',
-    documentation: 'Launch user app1'
-}, {
+    documentation: 'Launch user app1',
+  },
+  {
     label: '{LAUNCH_APP2}',
-    documentation: 'Launch user app2'
-}, {
+    documentation: 'Launch user app2',
+  },
+  {
     label: '{LAUNCH_MAIL}',
-    documentation: 'Launch the email application'
-}, {
+    documentation: 'Launch the email application',
+  },
+  {
     label: '{LAUNCH_MEDIA}',
-    documentation: 'Launch media player'
-}, {
+    documentation: 'Launch media player',
+  },
+  {
     label: '{LCTRL}',
-    documentation: 'Left CTRL key'
-}, {
+    documentation: 'Left CTRL key',
+  },
+  {
     label: '{LEFT}',
-    documentation: 'Left arrow'
-}, {
+    documentation: 'Left arrow',
+  },
+  {
     label: '{LSHIFT}',
-    documentation: 'Left Shift key'
-}, {
+    documentation: 'Left Shift key',
+  },
+  {
     label: '{LWIN}',
-    documentation: 'Left Windows key'
-}, {
+    documentation: 'Left Windows key',
+  },
+  {
     label: '{LWINDOWN}',
-    documentation: 'Holds the left Windows key down until {LWINUP} is sent'
-}, {
+    documentation: 'Holds the left Windows key down until {LWINUP} is sent',
+  },
+  {
     label: '{LWINUP}',
-    documentation: 'Releases the Windows key'
-}, {
+    documentation: 'Releases the Windows key',
+  },
+  {
     label: '{MEDIA_NEXT}',
-    documentation: 'Select next track in media player'
-}, {
+    documentation: 'Select next track in media player',
+  },
+  {
     label: '{MEDIA_PLAY_PAUSE}',
-    documentation: 'Play/pause media player'
-}, {
+    documentation: 'Play/pause media player',
+  },
+  {
     label: '{MEDIA_PREV}',
-    documentation: 'Select previous track in media player'
-}, {
+    documentation: 'Select previous track in media player',
+  },
+  {
     label: '{MEDIA_STOP}',
-    documentation: 'Stop media player'
-}, {
+    documentation: 'Stop media player',
+  },
+  {
     label: '{NUMLOCK}',
     documentation: 'NUMLOCK (on/off/toggle)',
-    inserText: new SnippetString('{NUMLOCK ${1:on/off/toggle}}')
-}, {
+    inserText: new SnippetString('{NUMLOCK ').appendChoice(['on', 'off', 'toggle']).appendText('}'),
+  },
+  {
     label: '{NUMPAD0}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD1}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD2}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD3}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD4}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD5}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD6}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD7}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD8}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPAD9}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{NUMPADADD}',
-    documentation: 'Numpad Add'
-}, {
+    documentation: 'Numpad Add',
+  },
+  {
     label: '{NUMPADDIV}',
-    documentation: 'Numpad Divide'
-}, {
+    documentation: 'Numpad Divide',
+  },
+  {
     label: '{NUMPADDOT}',
-    documentation: 'Numpad period'
-}, {
+    documentation: 'Numpad period',
+  },
+  {
     label: '{NUMPADENTER}',
-    documentation: 'Enter key on the numpad'
-}, {
+    documentation: 'Enter key on the numpad',
+  },
+  {
     label: '{NUMPADMULT}',
-    documentation: 'Numpad Multiply'
-}, {
+    documentation: 'Numpad Multiply',
+  },
+  {
     label: '{NUMPADSUB}',
-    documentation: 'Numpad Subtract'
-}, {
+    documentation: 'Numpad Subtract',
+  },
+  {
     label: '{OEM_102}',
-    documentation: 'Either the angle bracket key or the backslash key on the RT 102-key keyboard'
-}, {
+    documentation: 'Either the angle bracket key or the backslash key on the RT 102-key keyboard',
+  },
+  {
     label: '{PAUSE}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{PGDN}',
-    documentation: 'PageDown'
-}, {
+    documentation: 'PageDown',
+  },
+  {
     label: '{PGUP}',
-    documentation: 'PageUp'
-}, {
+    documentation: 'PageUp',
+  },
+  {
     label: '{PRINTSCREEN}',
-    documentation: 'Print Screen key'
-}, {
+    documentation: 'Print Screen key',
+  },
+  {
     label: '{RALT}',
-    documentation: 'Right ALT key'
-}, {
+    documentation: 'Right ALT key',
+  },
+  {
     label: '{RCTRL}',
-    documentation: 'Right CTRL key'
-}, {
+    documentation: 'Right CTRL key',
+  },
+  {
     label: '{RIGHT}',
-    documentation: 'Right arrow'
-}, {
+    documentation: 'Right arrow',
+  },
+  {
     label: '{RSHIFT}',
-    documentation: 'Right Shift key'
-}, {
+    documentation: 'Right Shift key',
+  },
+  {
     label: '{RWIN}',
-    documentation: 'Right Windows key'
-}, {
+    documentation: 'Right Windows key',
+  },
+  {
     label: '{RWINDOWN}',
-    documentation: 'Holds the right Windows key down until {RWINUP} is sent'
-}, {
+    documentation: 'Holds the right Windows key down until {RWINUP} is sent',
+  },
+  {
     label: '{RWINUP}',
-    documentation: 'Releases the right Windows key'
-}, {
+    documentation: 'Releases the right Windows key',
+  },
+  {
     label: '{SCROLLLOCK}',
     documentation: 'SCROLLLOCK (on/off/toggle)',
-    insertText: new SnippetString('{SCROLLLOCK ${1:on/off/toggle}}')
-}, {
+    inserText: new SnippetString('{SCROLLLOCK ')
+      .appendChoice(['on', 'off', 'toggle'])
+      .appendText('}'),
+  },
+  {
     label: '{SHIFTDOWN}',
-    documentation: 'Holds the SHIFT key down until {SHIFTUP} is sent'
-}, {
+    documentation: 'Holds the SHIFT key down until {SHIFTUP} is sent',
+  },
+  {
     label: '{SHIFTUP}',
-    documentation: 'Releases the SHIFT key'
-}, {
+    documentation: 'Releases the SHIFT key',
+  },
+  {
     label: '{SLEEP}',
-    documentation: 'Computer SLEEP key'
-}, {
+    documentation: 'Computer SLEEP key',
+  },
+  {
     label: '{SPACE}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{TAB}',
-    documentation: ''
-}, {
+    documentation: '',
+  },
+  {
     label: '{UP}',
-    documentation: 'Up arrow'
-}, {
+    documentation: 'Up arrow',
+  },
+  {
     label: '{VOLUME_DOWN}',
-    documentation: 'Reduce the volume'
-}, {
+    documentation: 'Reduce the volume',
+  },
+  {
     label: '{VOLUME_MUTE}',
-    documentation: 'Mute the volume'
-}, {
+    documentation: 'Mute the volume',
+  },
+  {
     label: '{VOLUME_UP}',
-    documentation: 'Increase the volume'
-}]
+    documentation: 'Increase the volume',
+  },
+];
 
 // Add the variable icon and Macro detail to each entry
-for (var i of items) {
-    i.kind = CompletionItemKind.Constant
-    i.detail = 'Send() Command'
-}
+const entries = fillCompletions(items, CompletionItemKind.Constant, 'Send() Command');
 
-module.exports = items
+export default entries;
