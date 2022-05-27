1c9dcbe4f624246f2a64d874382ec58962b56faa
Logan Hampton <losesmc@gmail.com>
2022-05-26 18:17:49 -0700

    refactor: use fillCompletions function

--- src/completions/constants_avi.js
+++ src/completions/constants_avi.js
@@ -1,33 +1,28 @@
-var {
-    CompletionItemKind
-} = require('vscode')
+import { CompletionItemKind } from 'vscode';
+import { fillCompletions } from '../util';
 
-var items = [{
-        label: '$ACS_AUTOPLAY',
-        documentation: 'Starts playing the animation as soon as the AVI clip is opened. ',
-        detail: 'Avi Clip Style Constant'
-    },
-    {
-        label: '$ACS_CENTER',
-        documentation: 'Centers the animation in the animation control\'s window. ',
-        detail: 'Avi Clip Style Constant'
-    },
-    {
-        label: '$ACS_TRANSPARENT',
-        documentation: 'Allows you to match an animation\'s background color to that of the underlying window, creating a \"transparent\" background. (Default value) ',
-        detail: 'Avi Clip Style Constant'
-    },
-    {
-        label: '$ACS_NONTRANSPARENT',
-        documentation: 'To override default ACS_TRANSPARENT ',
-        detail: 'Avi Clip Style Constant'
-    },
-]
+const items = [
+  {
+    label: '$ACS_AUTOPLAY',
+    documentation: 'Starts playing the animation as soon as the AVI clip is opened. ',
+  },
+  {
+    label: '$ACS_CENTER',
+    documentation: "Centers the animation in the animation control's window. ",
+  },
+  {
+    label: '$ACS_TRANSPARENT',
+    documentation:
+      'Allows you to match an animation\'s background color to that of the underlying window, creating a "transparent" background. (Default value) ',
+  },
+  {
+    label: '$ACS_NONTRANSPARENT',
+    documentation: 'To override default ACS_TRANSPARENT ',
+  },
+];
 
-// Add the icon and detail to each entry
-for (var i of items) {
-    i.kind = CompletionItemKind.Constant
-    i.documentation += '\n\nRequires #include <AVIConstants.au3>'
-}
-
-module.exports = items
+export default fillCompletions(
+  items,
+  CompletionItemKind.Constant,
+  'AVI Clip Style Constant - #include <AVIConstants.au3>',
+);
