a86efb34e5627ffbd7f4459c9d0d2eb3851a21f9
Logan Hampton <losesmc@gmail.com>
2022-10-17 17:23:10 -0700

    SymbolInformation for functions get full range
    For #128
    
    Enables Sticky Scroll as well as nested symbols in the outline.
    
    Previously, the SymbolInformation for functions were only providing the starting point for a function.

--- src/ai_symbols.js
+++ src/ai_symbols.js
@@ -1,10 +1,43 @@
-import { languages, Location, SymbolInformation, SymbolKind, workspace } from 'vscode';
-import { AI_CONSTANTS, AUTOIT_MODE, isSkippableLine, functionPattern, variablePattern, regionPattern } from './util';
+import { languages, Location, SymbolInformation, SymbolKind, workspace, Range } from 'vscode';
+import {
+  AI_CONSTANTS,
+  AUTOIT_MODE,
+  isSkippableLine,
+  functionPattern,
+  variablePattern,
+  regionPattern,
+} from './util';
 
 const config = workspace.getConfiguration('autoit');
 
 const createVariableSymbol = (variable, variableKind, doc, line) => {
   return new SymbolInformation(variable, variableKind, '', new Location(doc.uri, line.range));
+};
+
+/**
+ * Generates a SymbolInformation object for a function from a given TextDocument
+ * that includes the full range of the function's body
+ * @param {String} functionName The name of the function from the AutoIt script
+ * @param {TextDocument} doc The current document to search
+ * @param {String} docText The text from the document (usually generated through `TextDocument.getText()`)
+ * @returns SymbolInformation
+ */
+const createFunctionSymbol = (functionName, doc, docText) => {
+  const pattern = new RegExp(
+    // `^Func\\s+\\b(?<funcName>${functionName}\\b).*\\n(?:(?!EndFunc\\b).*\\n)*EndFunc.*\\n?`
+    `Func\\s+\\b(?<funcName>${functionName}+\\b).*?(EndFunc)`,
+    's',
+  );
+  const result = pattern.exec(docText);
+  const endPoint = result.index + result[0].length;
+  const newFunctionSymbol = new SymbolInformation(
+    result[1],
+    SymbolKind.Function,
+    '',
+    new Location(doc.uri, new Range(doc.positionAt(result.index), doc.positionAt(endPoint))),
+  );
+
+  return newFunctionSymbol;
 };
 
 export default languages.registerDocumentSymbolProvider(AUTOIT_MODE, {
@@ -19,7 +52,7 @@ export default languages.registerDocumentSymbolProvider(AUTOIT_MODE, {
     for (let lineNum = 0; lineNum < lineCount; lineNum += 1) {
       const line = doc.lineAt(lineNum);
       const { text } = line;
-			const regionName = text.match(regionPattern);
+      const regionName = text.match(regionPattern);
 
       if (isSkippableLine(line) && !regionName) {
         // eslint-disable-next-line no-continue
@@ -28,12 +61,7 @@ export default languages.registerDocumentSymbolProvider(AUTOIT_MODE, {
 
       funcName = functionPattern.exec(text);
       if (funcName && !found.includes(funcName[1])) {
-        const functionSymbol = new SymbolInformation(
-          funcName[1],
-          SymbolKind.Function,
-          '',
-          new Location(doc.uri, line.range),
-        );
+        const functionSymbol = createFunctionSymbol(funcName[1], doc, doc.getText());
         result.push(functionSymbol);
         found.push(funcName[1]);
       }
@@ -42,39 +70,39 @@ export default languages.registerDocumentSymbolProvider(AUTOIT_MODE, {
         const variables = text.match(variablePattern);
 
         if (variables) {
-					if (/^Const/.test(text)) {
-						variableKind = SymbolKind.Constant;
-					} else if (/^Enum/.test(text)) {
-						variableKind = SymbolKind.Enum;
-					} else {
-						variableKind = SymbolKind.Variable;
-					}
-					
-					// eslint-disable-next-line no-loop-func
-					variables.forEach(variable => {
-						if (found.includes(variable) || AI_CONSTANTS.includes(variable)) {
-							return;
-						}
-						
-						result.push(createVariableSymbol(variable, variableKind, doc, line));
-						found.push(variable);
-					});
-      	}
-			}
+          if (/^Const/.test(text)) {
+            variableKind = SymbolKind.Constant;
+          } else if (/^Enum/.test(text)) {
+            variableKind = SymbolKind.Enum;
+          } else {
+            variableKind = SymbolKind.Variable;
+          }
 
-			if (config.showRegionsInGoToSymbol) {
-				if (regionName && !found.includes(regionName[0])) {
-					const regionSymbol = new SymbolInformation(
-						regionName[1],
-						SymbolKind.Namespace,
-						'',
-						new Location(doc.uri, line.range),
-						);
-					result.push(regionSymbol);
-					found.push(regionName[1]);
-				}			
-			}
-		}
+          // eslint-disable-next-line no-loop-func
+          variables.forEach(variable => {
+            if (found.includes(variable) || AI_CONSTANTS.includes(variable)) {
+              return;
+            }
+
+            result.push(createVariableSymbol(variable, variableKind, doc, line));
+            found.push(variable);
+          });
+        }
+      }
+
+      if (config.showRegionsInGoToSymbol) {
+        if (regionName && !found.includes(regionName[0])) {
+          const regionSymbol = new SymbolInformation(
+            regionName[1],
+            SymbolKind.Namespace,
+            '',
+            new Location(doc.uri, line.range),
+          );
+          result.push(regionSymbol);
+          found.push(regionName[1]);
+        }
+      }
+    }
 
     return result;
   },
