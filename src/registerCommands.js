d20009a6015185fc12fddeabdf9f20256d491293
Logan Hampton <losesmc@gmail.com>
2022-10-24 13:01:43 -0700

    style: prettier/eslint changes

--- src/registerCommands.js
+++ src/registerCommands.js
@@ -1,9 +1,9 @@
 import { commands } from 'vscode';
 import * as aiCommands from './ai_commands';
-import { commandsList, commandsPrefix } from "./commandsList";
+import { commandsList, commandsPrefix } from './commandsList';
 
 export const registerCommands = () => {
-  for(let i = 0; i < commandsList.length; i++) {
+  for (let i = 0; i < commandsList.length; i += 1) {
     const command = commandsList[i];
     if (aiCommands[command])
       commands.registerCommand(commandsPrefix + command, aiCommands[command]);
